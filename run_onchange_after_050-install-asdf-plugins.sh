71dff8e5e73b3e4ec0ad3541f988c5e82928ec8f
KH (neumachen) <kareem@hepburn.info>
2022-03-25 18:58:39 -0500

    fix pre-installation of asdf plugins

--- run_onchange_after_050-install-asdf-plugins.sh
+++ run_onchange_after_050-install-asdf-plugins.sh
@@ -16,51 +16,51 @@ fi
 
 . "${HOME}/.asdf/asdf.sh"
 
-plugin_repos=(
-  "https://github.com/Antiarchitect/asdf-helm.git"
-  "https://github.com/Banno/asdf-kubectl.git"
-  "https://github.com/andweeb/asdf-delta.git"
-  "https://github.com/aphecetche/asdf-tmux.git"
-  "https://github.com/asdf-vm/asdf-elixir.git"
-  "https://github.com/asdf-vm/asdf-erlang.git"
-  "https://github.com/asdf-vm/asdf-nodejs.git"
-  "https://github.com/asdf-vm/asdf-ruby.git"
-  "https://github.com/azmcode/asdf-jq.git"
-  "https://github.com/bartlomiejdanek/asdf-github-cli.git"
-  "https://github.com/cLupus/asdf-sqlite.git"
-  "https://github.com/cmur2/asdf-broot.git"
-  "https://github.com/code-lever/asdf-rust.git"
-  "https://github.com/danhper/asdf-python.git"
-  "https://github.com/itspngu/asdf-usql.git"
-  "https://github.com/jfreeland/asdf-kubespy.git"
-  "https://github.com/johnlayton/asdf-trdsql.git"
-  "https://github.com/jthegedus/asdf-gcloud"
-  "https://github.com/kajisha/asdf-ghq.git"
-  "https://github.com/kennyp/asdf-golang.git"
-  "https://github.com/kompiro/asdf-fzf.git"
-  "https://github.com/looztra/asdf-gitui.git"
-  "https://github.com/looztra/asdf-k9s.git"
-  "https://github.com/luizm/asdf-shfmt.git"
-  "https://github.com/mattysweeps/asdf-concourse.git"
-  "https://github.com/nyrst/asdf-exa.git"
-  "https://github.com/richin13/asdf-neovim.git"
-  "https://github.com/spencergilbert/asdf-k3d.git"
-  "https://github.com/sudermanjr/asdf-yq.git"
-  "https://gitlab.com/craigfurman/asdf-go-jsonnet.git"
-  "https://gitlab.com/wt0f/asdf-bat.git"
-  "https://gitlab.com/wt0f/asdf-dyff.git"
-  "https://gitlab.com/wt0f/asdf-fd.git"
-  "https://gitlab.com/wt0f/asdf-kubectx"
-  "https://gitlab.com/wt0f/asdf-ripgrep.git"
+plugins=(
+  "bat;https://gitlab.com/wt0f/asdf-bat.git"
+  "broot;https://github.com/cmur2/asdf-broot.git"
+  "concourse;https://github.com/mattysweeps/asdf-concourse.git"
+  "delta;https://github.com/andweeb/asdf-delta.git"
+  "dyff;https://gitlab.com/wt0f/asdf-dyff.git"
+  "elixir;https://github.com/asdf-vm/asdf-elixir.git"
+  "erlang;https://github.com/asdf-vm/asdf-erlang.git"
+  "exa;https://github.com/nyrst/asdf-exa.git"
+  "fd;https://gitlab.com/wt0f/asdf-fd.git"
+  "fzf;https://github.com/kompiro/asdf-fzf.git"
+  "gcloud;https://github.com/jthegedus/asdf-gcloud"
+  "ghq;https://github.com/kajisha/asdf-ghq.git"
+  "github-cli;https://github.com/bartlomiejdanek/asdf-github-cli.git"
+  "gitui;https://github.com/looztra/asdf-gitui.git"
+  "go-jsonnet;https://gitlab.com/craigfurman/asdf-go-jsonnet.git"
+  "golang;https://github.com/kennyp/asdf-golang.git"
+  "helm;https://github.com/Antiarchitect/asdf-helm.git"
+  "jq;https://github.com/azmcode/asdf-jq.git"
+  "k3d;https://github.com/spencergilbert/asdf-k3d.git"
+  "k9s;https://github.com/looztra/asdf-k9s.git"
+  "kubectl;https://github.com/Banno/asdf-kubectl.git"
+  "kubectx;https://gitlab.com/wt0f/asdf-kubectx"
+  "kubespy;https://github.com/jfreeland/asdf-kubespy.git"
+  "neovim;https://github.com/richin13/asdf-neovim.git"
+  "nodejs;https://github.com/asdf-vm/asdf-nodejs.git"
+  "python;https://github.com/danhper/asdf-python.git"
+  "ripgrep;https://gitlab.com/wt0f/asdf-ripgrep.git"
+  "rub;https://github.com/asdf-vm/asdf-ruby.git"
+  "rust;https://github.com/code-lever/asdf-rust.git"
+  "shfmt;https://github.com/luizm/asdf-shfmt.git"
+  "sqlite;https://github.com/cLupus/asdf-sqlite.git"
+  "tmux;https://github.com/aphecetche/asdf-tmux.git"
+  "trdsql;https://github.com/johnlayton/asdf-trdsql.git"
+  "usql;https://github.com/itspngu/asdf-usql.git"
+  "yq;https://github.com/sudermanjr/asdf-yq.git"
 )
 
 if command -v asdf >/dev/null 2>&1; then
-  # add plugin repositories
-  for ((i = 0; i < ${#plugin_repos[@]}; i++)); do
-    plugin_repo="${plugin_repos[i]}"
-    echo-run asdf plugin add ${plugin_repo} || true
+  for ((i = 0; i < ${#plugins[@]}; i++)); do
+    IFS=';' read -r plugin_name plugin_repo <<<"${plugins[i]}"
+    echo-run asdf plugin add ${plugin_name} ${plugin_repo} || true
+    echo-run asdf install ${plugin_name} || true
+    unset IFS
   done
-  echo-run asdf install
 fi
 
 if [ -n "$GITHUB_WORKFLOW" ]; then
