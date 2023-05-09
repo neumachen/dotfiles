67d11b3b86dff07b8be0bfc626e61dee2679d9b1
KH (neumachen) <kareem@hepburn.info>
2023-05-09 09:10:35 -0700

    patch: modify mapping for Telescope

--- private_dot_config/exact_nvim/exact_after/plugin/telescope.lua
+++ private_dot_config/exact_nvim/exact_after/plugin/telescope.lua
@@ -7,8 +7,24 @@ telescope.load_extension("file_browser")
 local builtin = require('telescope.builtin')
 local keymap = vim.keymap
 
-keymap.set('n', '<leader>tf', builtin.find_files, {})
-keymap.set('n', '<leader>tg', builtin.live_grep, {})
-keymap.set('n', '<leader>tt', builtin.help_tags, {})
-keymap.set('n', '<leader>tu', builtin.buffers, {})
-keymap.set('n', '<leader>ts', builtin.git_status, {})
+keymap.set('n', '<space>ff', builtin.find_files, {})
+keymap.set('n', '<space>lg', builtin.live_grep, {})
+keymap.set('n', '<space>ht', builtin.help_tags, {})
+keymap.set('n', '<space>bs', builtin.buffers, {})
+keymap.set('n', '<space>gs', builtin.git_status, {})
+
+-- Open from the project directory
+vim.api.nvim_set_keymap(
+  "n",
+  "<space>pd",
+  ":Telescope file_browser",
+  { noremap = true }
+)
+
+-- open file_browser with the path of the current buffer
+vim.api.nvim_set_keymap(
+  "n",
+  "<space>fb",
+  ":Telescope file_browser path=%:p:h select_buffer=true",
+  { noremap = true }
+)
