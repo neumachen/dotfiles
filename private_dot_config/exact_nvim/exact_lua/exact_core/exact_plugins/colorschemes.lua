4f307b733c53a2112babb3d3cf5283007d70945e
KH (neumachen) <kareem@hepburn.info>
2023-06-04 13:27:45 -0500

    feature: switch over to lazyvim

---
+++ private_dot_config/exact_nvim/exact_lua/exact_core/exact_plugins/colorschemes.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "EdenEast/nightfox.nvim",
+    config = function()
+      require("core.plugins.themes.nightfox")
+    end,
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    config = function()
+      require("core.plugins.themes.catppuccin")
+    end,
+  },
+  {
+    "folke/tokyonight.nvim",
+    config = function()
+      require("core.plugins.themes.tokyonight")
+    end,
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    config = function()
+      require("core.plugins.themes.kanagawa")
+    end,
+  },
+  {
+    "sam4llis/nvim-tundra",
+    config = function()
+      require("core.plugins.themes.tundra")
+    end,
+  },
+  {
+    "shaunsingh/nord.nvim",
+  },
+}
