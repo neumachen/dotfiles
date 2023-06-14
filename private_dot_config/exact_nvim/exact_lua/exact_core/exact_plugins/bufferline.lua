d6a98f269f7fafb210e2a415521691246508e5a2
KH (neumachen) <kareem@hepburn.info>
2023-06-14 10:25:56 -0500

    patch: fix neo-tree config

--- private_dot_config/exact_nvim/exact_lua/exact_core/exact_plugins/bufferline.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_core/exact_plugins/bufferline.lua
@@ -2,6 +2,9 @@ local utils = require("core.utils.functions")
 
 return {
   "akinsho/bufferline.nvim",
+  dependencies = {
+    { "nvim-tree/nvim-web-devicons" },
+  },
   config = function()
     require("bufferline").setup({
       options = {
