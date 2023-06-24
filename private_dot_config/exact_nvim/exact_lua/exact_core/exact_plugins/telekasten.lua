26696b3289edbaab3ba13116776905a5a2878d98
KH (neumachen) <kareem@hepburn.info>
2023-06-24 17:57:15 -0500

    patch: configure peek

--- private_dot_config/exact_nvim/exact_lua/exact_core/exact_plugins/telekasten.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_core/exact_plugins/telekasten.lua
@@ -1,5 +1,9 @@
 return {
   "renerocksai/telekasten.nvim",
+  dependencies = {
+    "nvim-telescope/telescope-symbols.nvim",
+    "iamcco/markdown-preview.nvim",
+  },
   config = function()
     require("telekasten").setup({
       home = os.getenv("MEIN_WISSEN_PATH"), -- Put the name of your notes directory here
