10eab0228eba030101d1281357572d9880e07226
KH (neumachen) <kareem@hepburn.info>
2023-06-29 11:30:38 -0500

    feat: auto import and format go code

--- private_dot_config/exact_nvim/exact_after/ftplugin/go.lua
+++ private_dot_config/exact_nvim/exact_after/ftplugin/go.lua
@@ -3,4 +3,15 @@ vo.tabstop = 4
 vo.shiftwidth = 4
 vo.softtabstop = 4
 
+local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+   require('go.format').goimport()
+  end,
+  group = format_sync_grp,
+})
+
+
 require("core.plugins.dap.dap").setup()
