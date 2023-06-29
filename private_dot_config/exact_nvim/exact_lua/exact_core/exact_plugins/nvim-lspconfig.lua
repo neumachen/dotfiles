f2eb7b80b38270396ce43f6b5c450ad271666e86
KH (neumachen) <kareem@hepburn.info>
2023-06-29 11:31:22 -0500

    refactor: define lua_ls using runtime files

--- private_dot_config/exact_nvim/exact_lua/exact_core/exact_plugins/nvim-lspconfig.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_core/exact_plugins/nvim-lspconfig.lua
@@ -2,12 +2,9 @@ return {
   "neovim/nvim-lspconfig",
   dependencies = {
     { "onsails/lspkind-nvim" },
-    { "folke/neodev.nvim" },
     { "b0o/schemastore.nvim" },
   },
   config = function()
-    -- https://github.com/folke/neodev.nvim#-setup
-    require("neodev").setup()
     require("core.plugins.lsp.config")
   end,
 }
