610b52e54d790b4319b9a8008f10f579ce80e40a
KH (neumachen) <kareem@hepburn.info>
2023-06-29 11:31:04 -0500

    feat: add more colorschemes to try

---
+++ private_dot_config/exact_nvim/exact_lua/exact_core/exact_plugins/themes/neon.lua
@@ -0,0 +1,10 @@
+M = {}
+
+function M.setup()
+  vim.g.neon_style = "default"
+  vim.g.neon_italic_keyword = true
+  vim.g.neon_italic_function = true
+  vim.g.neon_transparent = true
+end
+
+return M
