cdff2872fd610dd7b324975f139b85e8e9ce64f2
KH (neumachen) <kareem@hepburn.info>
2023-08-02 15:29:47 -0500

    feat: configure java lsp, jdtls

--- private_dot_config/exact_nvim/exact_lua/exact_core/exact_plugins/lsp/servers/init.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_core/exact_plugins/lsp/servers/init.lua
@@ -1,4 +1,5 @@
 return {
+  jdtls = require("core.plugins.lsp.servers.jdtls"),
   jsonls = require("core.plugins.lsp.servers.jsonls"),
   lua_ls = require("core.plugins.lsp.servers.lua_ls"),
   pyright = require("core.plugins.lsp.servers.pyright"),
