b0ee5383bbb55968abb03e9ba7bb4d7b5c7e4db4
KH (neumachen) <kareem@hepburn.info>
2023-09-20 11:16:52 -0500

    feat: use tokyonight as theme

--- private_dot_config/exact_nvim/lua/as/plugins/themes.lua
+++ private_dot_config/exact_nvim/lua/as/plugins/themes.lua
@@ -9,4 +9,44 @@ return {
       vim.g.doom_one_pumblend_transparency = 3
     end,
   },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("tokyonight").setup({
+        -- borderless telescope
+        on_highlights = function(hl, c)
+          local prompt = "#2d3149"
+          hl.telescopenormal = {
+            bg = c.bg_dark,
+            fg = c.fg_dark,
+          }
+          hl.telescopeborder = {
+            bg = c.bg_dark,
+            fg = c.bg_dark,
+          }
+          hl.telescopepromptnormal = {
+            bg = prompt,
+          }
+          hl.telescopepromptborder = {
+            bg = prompt,
+            fg = prompt,
+          }
+          hl.telescopeprompttitle = {
+            bg = prompt,
+            fg = prompt,
+          }
+          hl.telescopepreviewtitle = {
+            bg = c.bg_dark,
+            fg = c.bg_dark,
+          }
+          hl.telescoperesultstitle = {
+            bg = c.bg_dark,
+            fg = c.bg_dark,
+          }
+        end,
+      })
+    end,
+  }
 }
