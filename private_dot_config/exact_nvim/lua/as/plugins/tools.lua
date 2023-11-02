e880aec29f32d3c8bdd233bbbb55a36c7a07b74d
KH (neumachen) <kareem@hepburn.info>
2023-11-02 11:07:51 -0500

    chore(nvim): format lua and add linters

--- private_dot_config/exact_nvim/lua/as/plugins/tools.lua
+++ private_dot_config/exact_nvim/lua/as/plugins/tools.lua
@@ -1,46 +1,58 @@
 return {
   {
-    'stevearc/conform.nvim',
-    event = 'BufReadPre',
+    "stevearc/conform.nvim",
+    event = "BufReadPre",
     opts = {
       formatters_by_ft = {
-        go = { 'goimports', 'gofumpt' },
-        javascript = { 'prettier' },
-        lua = { 'stylua' },
-        markdown = { 'prettier' },
-        pgsql = { 'sql_formatter' },
-        sh = { 'shfmt' },
-        sql = { 'sql_formatter' },
+        go = { "goimports", "gofumpt" },
+        javascript = { "prettier" },
+        lua = { "stylua" },
+        markdown = { "prettier" },
+        pgsql = { "sql_formatter" },
+        sh = { "shfmt" },
+        sql = { "sql_formatter" },
       },
       format_on_save = function(buf)
-        if vim.g.formatting_disabled or vim.b[buf].formatting_disabled then return end
+        if vim.g.formatting_disabled or vim.b[buf].formatting_disabled then
+          return
+        end
         return { timeout_ms = 500, lsp_fallback = true }
       end,
     },
     config = function(_, opts)
-      require('conform').setup(opts)
-      require('conform.formatters.sql_formatter').args = function(ctx)
-        local config_path = ctx.dirname .. '/.sql-formatter.json'
-        if vim.uv.fs_stat(config_path) then return { '--config', config_path } end
-        return { '--language', 'postgresql' }
+      require("conform").setup(opts)
+      require("conform.formatters.sql_formatter").args = function(ctx)
+        local config_path = ctx.dirname .. "/.sql-formatter.json"
+        if vim.uv.fs_stat(config_path) then
+          return { "--config", config_path }
+        end
+        return { "--language", "postgresql" }
       end
     end,
   },
   {
-    'mfussenegger/nvim-lint',
-    event = 'BufReadPre',
+    "mfussenegger/nvim-lint",
+    event = "BufReadPre",
     init = function()
-      vim.api.nvim_create_autocmd({ 'TextChanged' }, {
-        callback = function() require('lint').try_lint() end,
+      vim.api.nvim_create_autocmd({ "TextChanged" }, {
+        callback = function()
+          require("lint").try_lint()
+        end,
       })
     end,
     config = function()
-      require('lint').linters_by_ft = {
-        go = { 'golangcilint' },
-        javascript = { 'eslint' },
-        markdown = { 'markdownlint' },
-        sh = { 'shellharden' },
-        yaml = { 'yamllint' },
+      require("lint").linters_by_ft = {
+        dockerfile = { "hadolint" },
+        fennel = { "fennel" },
+        go = { "golangcilint" },
+        javascript = { "eslint" },
+        json = { "jsonlint" },
+        markdown = { "markdownlint" },
+        python = { "pylint" },
+        ruby = { "rubocop" },
+        sh = { "shellharden", "shellcheck" },
+        sql = { "sqlformatter" },
+        yaml = { "yamllint" },
       }
     end,
   },
