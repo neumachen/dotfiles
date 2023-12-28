834f7063c867f94116f7e6c4a4fc5202cb2fe1e1
KH (neumachen) <kareem@hepburn.info>
2023-12-28 09:16:05 -0600

    feat(nvim): ensure formatters are installed

--- private_dot_config/exact_nvim/lua/as/plugins/tools.lua
+++ private_dot_config/exact_nvim/lua/as/plugins/tools.lua
@@ -1,90 +1,94 @@
 return {
   {
-    "terrastruct/d2-vim",
+    'terrastruct/d2-vim',
   },
   {
-    "jackMort/ChatGPT.nvim",
-    cmd = { "ChatGPT", "ChatGPTActAs", "ChatGPTEditWithInstructions" },
+    'jackMort/ChatGPT.nvim',
+    cmd = { 'ChatGPT', 'ChatGPTActAs', 'ChatGPTEditWithInstructions' },
     config = function()
-      local border = { style = as.ui.border.rectangle, highlight = "PickerBorder" }
-      require("chatgpt").setup({
+      local border = { style = as.ui.border.rectangle, highlight = 'PickerBorder' }
+      require('chatgpt').setup({
         popup_window = { border = border },
-        popup_input = { border = border, submit = "<C-s>" },
+        popup_input = { border = border, submit = '<C-s>' },
         settings_window = { border = border },
         chat = {
           keymaps = {
             close = {
-              "<C-c>",--[[ , '<Esc>' ]]
+              '<C-c>',--[[ , '<Esc>' ]]
             },
           },
         },
       })
     end,
     dependencies = {
-      "MunifTanjim/nui.nvim",
-      "nvim-lua/plenary.nvim",
+      'MunifTanjim/nui.nvim',
+      'nvim-lua/plenary.nvim',
     },
   },
   {
-    "stevearc/conform.nvim",
-    event = "BufReadPre",
+    'stevearc/conform.nvim',
+    event = 'BufReadPre',
     opts = {
       formatters_by_ft = {
-        go = { "goimports", "gofumpt" },
-        javascript = { "prettier" },
-        lua = { "stylua" },
-        markdown = { "prettier" },
-        pgsql = { "sql_formatter" },
-        sh = { "shfmt" },
-        sql = { "sql_formatter" },
+        go = { 'goimports', 'gofumpt' },
+        javascript = { { 'prettierd', 'prettier' } },
+        lua = { 'stylua' },
+        markdown = { 'prettier' },
+        pgsql = { 'sql_formatter' },
+        sh = { 'shfmt' },
+        sql = { 'sql_formatter' },
       },
       format_on_save = function(buf)
-        if vim.g.formatting_disabled or vim.b[buf].formatting_disabled then
-          return
-        end
+        if vim.g.formatting_disabled or vim.b[buf].formatting_disabled then return end
         return { timeout_ms = 500, lsp_fallback = true }
       end,
     },
     config = function(_, opts)
-      require("conform").setup(opts)
-      require("conform.formatters.sql_formatter").args = function(ctx)
-        local config_path = ctx.dirname .. "/.sql-formatter.json"
-        if vim.uv.fs_stat(config_path) then
-          return { "--config", config_path }
-        end
-        return { "--language", "postgresql" }
+      require('conform').setup(opts)
+      require('conform.formatters.sql_formatter').args = function(ctx)
+        local config_path = ctx.dirname .. '/.sql-formatter.json'
+        if vim.uv.fs_stat(config_path) then return { '--config', config_path } end
+        return { '--language', 'postgresql' }
       end
     end,
   },
   {
-    "mfussenegger/nvim-lint",
-    event = "BufReadPre",
+    'mfussenegger/nvim-lint',
+    event = 'BufReadPre',
     init = function()
-      vim.api.nvim_create_autocmd({ "TextChanged" }, {
-        callback = function()
-          require("lint").try_lint()
-        end,
+      vim.api.nvim_create_autocmd({ 'TextChanged' }, {
+        callback = function() require('lint').try_lint() end,
       })
     end,
     config = function()
-      require("lint").linters_by_ft = {
-        gitcommit = { "commitlint" },
-        dockerfile = { "hadolint" },
-        fennel = { "fennel" },
-        go = { "golangcilint" },
-        javascript = { "eslint" },
-        json = { "jsonlint" },
-        markdown = { "vale" },
-        python = { "pylint" },
-        ruby = { "rubocop" },
-        sh = { "shellharden", "shellcheck" },
-        sql = { "sqlformatter" },
-        yaml = { "yamllint" },
+      require('lint').linters_by_ft = {
+        gitcommit = { 'commitlint' },
+        dockerfile = { 'hadolint' },
+        fennel = { 'fennel' },
+        go = { 'golangcilint' },
+        javascript = { 'eslint' },
+        json = { 'jsonlint' },
+        markdown = { 'vale' },
+        python = { 'pylint' },
+        ruby = { 'rubocop' },
+        sh = { 'shellharden', 'shellcheck' },
+        sql = { 'sqlformatter' },
+        yaml = { 'yamllint' },
       }
-      require("mason-nvim-lint").setup()
+      require('mason-nvim-lint').setup({
+        ensure_installed = {
+          'actionlint',
+          'commitlint',
+          'eslint_d',
+          'fennel',
+          'golangci-lint',
+          'hadolint',
+          'sql-formatter',
+        },
+      })
     end,
     dependencies = {
-      "rshkarin/mason-nvim-lint",
+      'rshkarin/mason-nvim-lint',
     },
   },
 }
