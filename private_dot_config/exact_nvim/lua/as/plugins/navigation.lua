df49c2d67ee57939bcb652e8d89ad0f3b452ec4a
KH (neumachen) <kareem@hepburn.info>
2024-05-24 16:04:48 -0500

    feat(nvim): use plugin for folder navigation

--- private_dot_config/exact_nvim/lua/as/plugins/navigation.lua
+++ private_dot_config/exact_nvim/lua/as/plugins/navigation.lua
@@ -4,109 +4,104 @@ local icons = as.ui.icons
 local autocmd = api.nvim_create_autocmd
 
 return {
+  { 'stevearc/oil.nvim'
+    init = function ()
+      require("oil").setup()
+    end,
+  },
   {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    cmd = { "Neotree" },
-    keys = { { "<C-N>", "<Cmd>Neotree toggle reveal<CR>", desc = "NeoTree" } },
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
+    cmd = { 'Neotree' },
+    keys = { { '<C-N>', '<Cmd>Neotree toggle reveal<CR>', desc = 'NeoTree' } },
     init = function()
-      autocmd("BufEnter", {
-        desc = "Load NeoTree if entering a directory",
+      autocmd('BufEnter', {
+        desc = 'Load NeoTree if entering a directory',
         callback = function(args)
           if fn.isdirectory(api.nvim_buf_get_name(args.buf)) > 0 then
-            require("lazy").load({ plugins = { "neo-tree.nvim" } })
+            require('lazy').load({ plugins = { 'neo-tree.nvim' } })
             api.nvim_del_autocmd(args.id)
           end
         end,
       })
     end,
     config = function()
-      highlight.plugin("NeoTree", {
+      highlight.plugin('NeoTree', {
         theme = {
-          ["*"] = {
-            { NeoTreeNormal = { link = "PanelBackground" } },
-            { NeoTreeNormalNC = { link = "PanelBackground" } },
-            { NeoTreeCursorLine = { link = "Visual" } },
+          ['*'] = {
+            { NeoTreeNormal = { link = 'PanelBackground' } },
+            { NeoTreeNormalNC = { link = 'PanelBackground' } },
+            { NeoTreeCursorLine = { link = 'Visual' } },
             { NeoTreeRootName = { underline = true } },
-            { NeoTreeStatusLine = { link = "PanelSt" } },
-            { NeoTreeTabActive = { bg = { from = "PanelBackground" }, bold = true } },
+            { NeoTreeStatusLine = { link = 'PanelSt' } },
+            { NeoTreeTabActive = { bg = { from = 'PanelBackground' }, bold = true } },
             {
               NeoTreeTabInactive = {
-                bg = { from = "PanelDarkBackground", alter = 0.15 },
-                fg = { from = "Comment" },
+                bg = { from = 'PanelDarkBackground', alter = 0.15 },
+                fg = { from = 'Comment' },
               },
             },
             {
-              NeoTreeTabSeparatorActive = { inherit = "PanelBackground", fg = { from = "Comment" } },
+              NeoTreeTabSeparatorActive = { inherit = 'PanelBackground', fg = { from = 'Comment' } },
             },
             -- stylua: ignore
             { NeoTreeTabSeparatorInactive = { inherit = 'NeoTreeTabInactive', fg = { from = 'PanelDarkBackground', attr = 'bg' } } },
           },
           -- NOTE: panel background colours don't get ignored by tint.nvim so avoid using them for now
           horizon = {
-            { NeoTreeWinSeparator = { link = "WinSeparator" } },
-            { NeoTreeTabActive = { link = "VisibleTab" } },
-            { NeoTreeTabSeparatorActive = { link = "VisibleTab" } },
-            { NeoTreeTabInactive = { inherit = "Comment", italic = false } },
-            { NeoTreeTabSeparatorInactive = { bg = "bg", fg = "bg" } },
+            { NeoTreeWinSeparator = { link = 'WinSeparator' } },
+            { NeoTreeTabActive = { link = 'VisibleTab' } },
+            { NeoTreeTabSeparatorActive = { link = 'VisibleTab' } },
+            { NeoTreeTabInactive = { inherit = 'Comment', italic = false } },
+            { NeoTreeTabSeparatorInactive = { bg = 'bg', fg = 'bg' } },
           },
         },
       })
 
-      local symbols = require("lspkind").symbol_map
+      local symbols = require('lspkind').symbol_map
       local lsp_kinds = as.ui.lsp.highlights
 
-      require("neo-tree").setup({
-        sources = { "filesystem", "git_status", "document_symbols" },
+      require('neo-tree').setup({
+        sources = { 'filesystem', 'git_status', 'document_symbols' },
         source_selector = {
           winbar = true,
-          separator_active = "",
+          separator_active = '',
           sources = {
-            { source = "filesystem" },
-            { source = "git_status" },
-            { source = "document_symbols" },
+            { source = 'filesystem' },
+            { source = 'git_status' },
+            { source = 'document_symbols' },
           },
         },
         enable_git_status = true,
         enable_normal_mode_for_inputs = true,
         git_status_async = true,
         nesting_rules = {
-          ["dart"] = { "freezed.dart", "g.dart" },
+          ['dart'] = { 'freezed.dart', 'g.dart' },
         },
         event_handlers = {
           {
-            event = "neo_tree_buffer_enter",
-            handler = function()
-              highlight.set("Cursor", { blend = 100 })
-            end,
+            event = 'neo_tree_buffer_enter',
+            handler = function() highlight.set('Cursor', { blend = 100 }) end,
           },
           {
-            event = "neo_tree_popup_buffer_enter",
-            handler = function()
-              highlight.set("Cursor", { blend = 0 })
-            end,
+            event = 'neo_tree_popup_buffer_enter',
+            handler = function() highlight.set('Cursor', { blend = 0 }) end,
           },
           {
-            event = "neo_tree_buffer_leave",
-            handler = function()
-              highlight.set("Cursor", { blend = 0 })
-            end,
+            event = 'neo_tree_buffer_leave',
+            handler = function() highlight.set('Cursor', { blend = 0 }) end,
           },
           {
-            event = "neo_tree_popup_buffer_leave",
-            handler = function()
-              highlight.set("Cursor", { blend = 100 })
-            end,
+            event = 'neo_tree_popup_buffer_leave',
+            handler = function() highlight.set('Cursor', { blend = 100 }) end,
           },
           {
-            event = "neo_tree_window_after_close",
-            handler = function()
-              highlight.set("Cursor", { blend = 0 })
-            end,
+            event = 'neo_tree_window_after_close',
+            handler = function() highlight.set('Cursor', { blend = 0 }) end,
           },
         },
         filesystem = {
-          hijack_netrw_behavior = "open_current",
+          hijack_netrw_behavior = 'open_current',
           use_libuv_file_watcher = true,
           group_empty_dirs = false,
           follow_current_file = {
@@ -117,12 +112,12 @@ return {
             visible = true,
             hide_dotfiles = false,
             hide_gitignored = true,
-            never_show = { ".DS_Store" },
+            never_show = { '.DS_Store' },
           },
           window = {
             mappings = {
-              ["/"] = "noop",
-              ["g/"] = "fuzzy_finder",
+              ['/'] = 'noop',
+              ['g/'] = 'fuzzy_finder',
             },
           },
         },
@@ -136,7 +131,7 @@ return {
               return acc
             end, symbols),
           },
-          modified = { symbol = icons.misc.circle .. " " },
+          modified = { symbol = icons.misc.circle .. ' ' },
           git_status = {
             symbols = {
               added = icons.git.add,
@@ -156,30 +151,31 @@ return {
         },
         window = {
           mappings = {
-            ["o"] = "toggle_node",
-            ["<CR>"] = "open_with_window_picker",
-            ["<c-s>"] = "split_with_window_picker",
-            ["<c-v>"] = "vsplit_with_window_picker",
-            ["<esc>"] = "revert_preview",
-            ["P"] = { "toggle_preview", config = { use_float = false } },
+            ['o'] = 'toggle_node',
+            ['<CR>'] = 'open_with_window_picker',
+            ['<c-s>'] = 'split_with_window_picker',
+            ['<c-v>'] = 'vsplit_with_window_picker',
+            ['<esc>'] = 'revert_preview',
+            ['P'] = { 'toggle_preview', config = { use_float = false } },
           },
         },
       })
     end,
     dependencies = {
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "nvim-tree/nvim-web-devicons",
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+      'nvim-tree/nvim-web-devicons',
       {
-        "ten3roberts/window-picker.nvim",
-        name = "window-picker",
+        'ten3roberts/window-picker.nvim',
+        name = 'window-picker',
         config = function()
-          local picker = require("window-picker")
+          local picker = require('window-picker')
           picker.setup()
           picker.pick_window = function()
-            return picker.select({ hl = "WindowPicker", prompt = "Pick window: " }, function(winid)
-              return winid or nil
-            end)
+            return picker.select(
+              { hl = 'WindowPicker', prompt = 'Pick window: ' },
+              function(winid) return winid or nil end
+            )
           end
         end,
       },
