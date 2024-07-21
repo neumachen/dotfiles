df6ea60bd076787ee8a56e85d4584187000e2b58
KH (neumachen) <kareem@hepburn.info>
2024-07-21 00:31:30 -0500

    patch(nvim): disable colorscheme overrides

--- private_dot_config/exact_nvim/lua/as/plugins/themes.lua
+++ private_dot_config/exact_nvim/lua/as/plugins/themes.lua
@@ -1,43 +1,7 @@
 return {
-  { 'akinsho/horizon.nvim', dev = true, lazy = false, priority = 1000 },
   {
     'folke/tokyonight.nvim',
     lazy = false,
     priority = 1000,
-    config = function()
-      require('tokyonight').setup({
-        -- borderless telescope
-        on_highlights = function(hl, c)
-          local prompt = '#2d3149'
-          hl.telescopenormal = {
-            bg = c.bg_dark,
-            fg = c.fg_dark,
-          }
-          hl.telescopeborder = {
-            bg = c.bg_dark,
-            fg = c.bg_dark,
-          }
-          hl.telescopepromptnormal = {
-            bg = prompt,
-          }
-          hl.telescopepromptborder = {
-            bg = prompt,
-            fg = prompt,
-          }
-          hl.telescopeprompttitle = {
-            bg = prompt,
-            fg = prompt,
-          }
-          hl.telescopepreviewtitle = {
-            bg = c.bg_dark,
-            fg = c.bg_dark,
-          }
-          hl.telescoperesultstitle = {
-            bg = c.bg_dark,
-            fg = c.bg_dark,
-          }
-        end,
-      })
-    end,
   },
 }
