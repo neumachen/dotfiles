d7f58d5d3e0fc6843b6016ab65500d9180dea37c
KH (neumachen) <kareem@hepburn.info>
2024-12-19 08:43:56 -0600

    fix(nvim): Update configuration and plugin settings

--- private_dot_config/exact_nvim/lua/as/plugins/ui.lua
+++ private_dot_config/exact_nvim/lua/as/plugins/ui.lua
@@ -1,6 +1,7 @@
+---@diagnostic disable: missing-fields
 local api, fn = vim.api, vim.fn
 local highlight, ui, falsy, augroup = as.highlight, as.ui, as.falsy, as.augroup
-local icons, border, rect = ui.icons.lsp, ui.current.border, ui.border.rectangle
+local icons, border = ui.icons.lsp, ui.current.border
 
 local lspkind = require('lspkind')
 
@@ -25,7 +26,7 @@ return {
         { DropBarMenuNormalFloat = { inherit = 'Pmenu' } },
       })
     end,
-    opts = {
+    bar = {
       general = {
         update_interval = 100,
         enable = function(buf, win)
@@ -313,46 +314,41 @@ return {
     end,
   },
   {
-    'folke/trouble.nvim',
+    "folke/trouble.nvim",
     lazy = false,
     opts = {}, -- for default options, refer to the configuration section for custom setup.
-    cmd = 'Trouble',
+    cmd = "Trouble",
     keys = {
       {
-        '<leader>xx',
-        '<cmd>Trouble diagnostics toggle<cr>',
-        desc = 'Diagnostics (Trouble)',
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
       },
       {
-        '<leader>xX',
-        '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
-        desc = 'Buffer Diagnostics (Trouble)',
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
       },
       {
-        '<leader>cs',
-        '<cmd>Trouble symbols toggle focus=false<cr>',
-        desc = 'Symbols (Trouble)',
+        "<leader>cs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
       },
       {
-        '<leader>cl',
-        '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
-        desc = 'LSP Definitions / references / ... (Trouble)',
+        "<leader>cl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
       },
       {
-        '<leader>xL',
-        '<cmd>Trouble loclist toggle<cr>',
-        desc = 'Location List (Trouble)',
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
       },
       {
-        '<leader>xQ',
-        '<cmd>Trouble qflist toggle<cr>',
-        desc = 'Quickfix List (Trouble)',
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
       },
     },
-  },
-  {
-    'andrewferrier/wrapping.nvim',
-    lzy = false,
-    config = function() require('wrapping').setup() end,
   },
 }
