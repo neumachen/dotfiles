2feb9269e2ef8e0bc4552cd2e013f2df12542aaa
KH (neumachen) <kareem@hepburn.info>
2024-12-22 16:03:28 -0600

    feat(editing): Add Nerdy plugin for icon selection and browsing

--- private_dot_config/exact_nvim/lua/as/plugins/editing.lua
+++ private_dot_config/exact_nvim/lua/as/plugins/editing.lua
@@ -74,4 +74,12 @@ return {
       })
     end,
   },
+  {
+      '2kabhishek/nerdy.nvim',
+      dependencies = {
+          'stevearc/dressing.nvim',
+          'nvim-telescope/telescope.nvim',
+      },
+      cmd = 'Nerdy',
+  },
 }
