d3a6b199e183a6747fdc342c7a36a9b91828065d
KH (neumachen) <kareem@hepburn.info>
2025-01-29 14:07:40 -0600

    refactor(nvim): Switch color scheme and update toggle.nvim configuration

--- private_dot_config/exact_nvim/lua/as/plugins/init.lua
+++ private_dot_config/exact_nvim/lua/as/plugins/init.lua
@@ -29,8 +29,22 @@ return {
   {
     'gregorias/toggle.nvim',
     lazy = false,
-    version = '2.0',
-    config = true,
+    init = function()
+      require('toggle').setup({
+        keymaps = {
+          toggle_option_prefix = 'yo',
+          previous_option_prefix = '[o',
+          next_option_prefix = ']o',
+          status_dashboard = 'yos',
+        },
+        -- The interface for registering keymaps.
+        keymap_registry = require('toggle.keymap').keymap_registry(),
+        -- See the default options section below.
+        -- options_by_keymap = …,
+        --- Whether to notify when a default option is set.
+        notify_on_set_default_option = true,
+      })
+    end,
   },
   {
     'm4xshen/hardtime.nvim',
