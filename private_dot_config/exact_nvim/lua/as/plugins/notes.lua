8936abd78624275763fb34e49307c3ddc08d5ca1
KH (neumachen) <kareem@hepburn.info>
2025-02-16 14:38:28 -0600

    refactor(notes): Replace Telekasten with Obsidian.nvim for note management

--- private_dot_config/exact_nvim/lua/as/plugins/notes.lua
+++ private_dot_config/exact_nvim/lua/as/plugins/notes.lua
@@ -1,74 +1,32 @@
-local mein_wissen_path = vim.g.mein_wissen_path
-if as.falsy(mein_wissen_path) then return {} end
+local denkwerstatt_dir = vim.g.denkwerstatt_dir
 
-local fmt = string.format
-local highlight = as.highlight
-local function mein_wissen_dir(path) return fmt('%s/%s', mein_wissen_path, path) end
+if as.falsy(denkwerstatt_dir) then return {} end
+
+local function denkwerstatt_path(path) return string.format('%s/%s', denkwerstatt_dir, path) end
 
 return {
   {
-    'renerocksai/telekasten.nvim',
-    lazy = false,
+    'epwalsh/obsidian.nvim',
+    ft = 'markdown',
     dependencies = {
-      'nvim-telescope/telescope.nvim',
-      'nvim-telescope/telescope-symbols.nvim',
-      'iamcco/markdown-preview.nvim',
-      'renerocksai/calendar-vim',
+      'nvim-lua/plenary.nvim',
     },
-    config = function()
-      require('telekasten').setup({
-        -- Main paths
-        home = mein_wissen_dir('notes'),
-        dailies = mein_wissen_dir('notes/daily'),
-        weeklies = mein_wissen_dir('notes/weekly'),
-        templates = mein_wissen_dir('notes/templates'),
-
-        template_new_note = mein_wissen_dir('notes/templates/new_note_tk.md'),
-        template_new_daily = mein_wissen_dir('notes/templates/daily_tk.md'),
-        template_new_weekly = mein_wissen_dir('notes/templates/weekly_tk.md'),
-
-        -- Generate note filenames. One of:
-        -- "title" (default) - Use title if supplied, uuid otherwise
-        -- "uuid" - Use uuid
-        -- "uuid-title" - Prefix title by uuid
-        -- "title-uuid" - Suffix title with uuid
-        new_note_filename = 'uuid-title',
-        filename_space_subst = '_',
-        new_note_location = 'smart',
-        -- file uuid type ("rand" or input for os.date such as "%Y%m%d%H%M")
-        uuid_type = os.time(os.date('!*t')),
-        -- UUID separator
-        uuid_sep = '_',
-
-        image_subdir = 'images',
-        image_link_style = 'markdown',
-        take_over_my_home = false,
-        auto_set_filetype = false,
-        auto_set_syntax = true,
-        install_syntax = true,
-      })
-
-      -- Launch panel if nothing is typed after <leader>z
-      map('n', '<leader>z', '<cmd>Telekasten panel<CR>')
-
-      -- Most used functions
-      map('n', '<leader>zf', '<cmd>Telekasten find_notes<CR>')
-      map('n', '<leader>zg', '<cmd>Telekasten search_notes<CR>')
-      map('n', '<leader>zd', '<cmd>Telekasten goto_today<CR>')
-      map('n', '<leader>zz', '<cmd>Telekasten follow_link<CR>')
-      map('n', '<leader>zn', '<cmd>Telekasten new_note<CR>')
-      map('n', '<leader>zc', '<cmd>Telekasten show_calendar<CR>')
-      map('n', '<leader>zb', '<cmd>Telekasten show_backlinks<CR>')
-      map('n', '<leader>zI', '<cmd>Telekasten insert_img_link<CR>')
-
-      -- Call insert link automatically when we start typing a link
-      map('i', '[[', '<cmd>Telekasten insert_link<CR>')
-    end,
+    opts = {
+      workspaces = {
+        {
+          name = 'personal',
+          path = denkwerstatt_path('personal'),
+        },
+        {
+          name = 'work',
+          path = denkwerstatt_path('arbeit'),
+        },
+      },
+    },
   },
   {
     'MeanderingProgrammer/render-markdown.nvim',
-    lazy = false,
-    enabled = true,
+    ft = 'markdown',
     dependencies = {
       'nvim-treesitter/nvim-treesitter',
       'nvim-tree/nvim-web-devicons',
