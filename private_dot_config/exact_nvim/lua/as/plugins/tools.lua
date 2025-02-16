c57b6bdff3ee730386a7d77f043c75ec7c3dadb8
KH (neumachen) <kareem@hepburn.info>
2025-02-16 13:51:02 -0600

    feat(nvim): Add chezmoi.nvim plugin for managing dotfiles in Neovim

--- private_dot_config/exact_nvim/lua/as/plugins/tools.lua
+++ private_dot_config/exact_nvim/lua/as/plugins/tools.lua
@@ -50,4 +50,26 @@ return {
       }
     end,
   },
+  {
+    'xvzc/chezmoi.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('chezmoi').setup({
+        {
+          edit = {
+            watch = true,
+            force = false,
+          },
+          notification = {
+            on_open = true,
+            on_apply = true,
+            on_watch = true,
+          },
+          telescope = {
+            select = { '<CR>' },
+          },
+        },
+      })
+    end,
+  },
 }
