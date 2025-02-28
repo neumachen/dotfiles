de8183edcb4bc5e0e5fd4ba724b5b375b344feaf
KH (neumachen) <kareem@hepburn.info>
2025-02-28 09:53:54 -0600

    feat(zsh): Add custom zmodule function and logging utilities

---
+++ private_dot_config/exact_zsh/functions/command-exists.zsh
@@ -0,0 +1,3 @@
+# ZSH only and most performant way to check existence of an executable
+# https://www.topbug.net/blog/2016/10/11/speed-test-check-the-existence-of-a-command-in-bash-and-zsh/
+function command_exists() { (( $+commands[$1] )); }
