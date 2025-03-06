af8af7556b8400fc5e7d1ef3cd52ee2e173abe8e
KH (neumachen) <kareem@hepburn.info>
2025-03-06 13:14:18 -0600

    refactor(nvim): Update plugin configurations and environment variable

--- private_dot_config/exact_nvim/lua/as/plugins/notes.lua
+++ private_dot_config/exact_nvim/lua/as/plugins/notes.lua
@@ -7,48 +7,49 @@ local function wissensspeicher_path(path) return string.format('%s/%s', wissenss
 return {
   {
     'epwalsh/obsidian.nvim',
-    ft = 'markdown',
     dependencies = {
       'nvim-lua/plenary.nvim',
       'nvim-telescope/telescope.nvim',
     },
-    opts = {
-      log_level = vim.log.levels.INFO,
-      completion = {
-        -- Set to false to disable completion.
-        nvim_cmp = true,
-        -- Trigger completion at 2 chars.
-        min_chars = 2,
-      },
-      workspaces = {
-        {
-          name = 'personal',
-          path = wissensspeicher_path('personlich'),
+    config = function()
+      require('obsidian').setup({
+        log_level = vim.log.levels.INFO,
+        completion = {
+          -- Set to false to disable completion.
+          nvim_cmp = true,
+          -- Trigger completion at 2 chars.
+          min_chars = 2,
         },
-        {
-          name = 'work',
-          path = wissensspeicher_path('arbeit'),
+        workspaces = {
+          {
+            name = 'personal',
+            path = wissensspeicher_path('personlich'),
+          },
+          {
+            name = 'work',
+            path = wissensspeicher_path('arbeit'),
+          },
         },
-      },
-      picker = {
-        -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
-        name = 'telescope.nvim',
-        -- Optional, configure key mappings for the picker. These are the defaults.
-        -- Not all pickers support all mappings.
-        note_mappings = {
-          -- Create a new note from your query.
-          new = '<C-x>',
-          -- Insert a link to the selected note.
-          insert_link = '<C-l>',
+        picker = {
+          -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
+          name = 'telescope.nvim',
+          -- Optional, configure key mappings for the picker. These are the defaults.
+          -- Not all pickers support all mappings.
+          note_mappings = {
+            -- Create a new note from your query.
+            new = '<C-x>',
+            -- Insert a link to the selected note.
+            insert_link = '<C-l>',
+          },
+          tag_mappings = {
+            -- Add tag(s) to current note.
+            tag_note = '<C-x>',
+            -- Insert a tag at the current location.
+            insert_tag = '<C-l>',
+          },
         },
-        tag_mappings = {
-          -- Add tag(s) to current note.
-          tag_note = '<C-x>',
-          -- Insert a tag at the current location.
-          insert_tag = '<C-l>',
-        },
-      },
-    },
+      })
+    end,
   },
   {
     'MeanderingProgrammer/render-markdown.nvim',
