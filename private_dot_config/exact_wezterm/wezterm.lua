8262a5b86212a7e9c0004b023c54b4605d537d50
KH (neumachen) <kareem@hepburn.info>
2025-03-08 12:48:12 -0600

    feat(wezterm): Update keybindings and workspace switcher configuration

--- private_dot_config/exact_wezterm/wezterm.lua
+++ private_dot_config/exact_wezterm/wezterm.lua
@@ -143,7 +143,7 @@ local config = {
   },
   -- separate <Tab> <C-i>
   enable_csi_u_key_encoding = true,
-  leader = { key = 'Space', mods = 'CTRL|SHIFT' },
+  leader = { key = 'SPACE', mods = 'SHIFT' },
   keys = keybinds.create_keybinds(),
   key_tables = keybinds.key_tables,
   mouse_bindings = keybinds.mouse_bindings,
