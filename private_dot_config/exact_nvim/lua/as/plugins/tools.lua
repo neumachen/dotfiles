f4e877a04aded3d65f8517cf7369f35c08ff6af3
KH (neumachen) <kareem@hepburn.info>
2025-03-11 09:28:08 -0500

    feat(nvim): Add package-info.nvim plugin for npm package management

--- private_dot_config/exact_nvim/lua/as/plugins/tools.lua
+++ private_dot_config/exact_nvim/lua/as/plugins/tools.lua
@@ -87,4 +87,9 @@ return {
       end,
     },
   },
+  {
+    'vuki656/package-info.nvim',
+    dependencies = { 'MunifTanjim/nui.nvim' },
+    config = function() require('package-info').setup() end,
+  },
 }
