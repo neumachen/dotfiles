ff6bf5f5af5ff1af03ba983bd740a76b3e21be14
Kareem Hepburn <kareem@hepburn.info>
2025-05-21 10:31:13 -0500

    chore(nvim): reorganize plugin configuration and remove unused files

---
+++ private_dot_config/exact_nvim/exact_lua/exact_config/exact_plugins/chezmoi.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    'xvzc/chezmoi.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope.nvim',
+    },
+    config = function()
+      require('chezmoi').setup({
+        {
+          edit = {
+            watch = true,
+            force = false,
+          },
+          notification = {
+            on_open = true,
+            on_apply = true,
+            on_watch = true,
+          },
+          telescope = {
+            select = { '<CR>' },
+          },
+        },
+      })
+
+      local telescope = require('telescope')
+      map('n', '<localleader>fcd', telescope.extensions.chezmoi.find_files, {
+        desc = 'edit dotfiles',
+      })
+    end,
+  },
+}
---
+++ private_dot_config/exact_nvim/exact_lua/exact_config/exact_plugins/nerdy.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    '2kabhishek/nerdy.nvim',
+    dependencies = {
+      'folke/snacks.nvim',
+    },
+    cmd = 'Nerdy',
+  },
+}
