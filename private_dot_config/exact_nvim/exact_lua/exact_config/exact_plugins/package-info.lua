d487ece7ce6abb2899245e54eaea3d4389c85545
Kareem Hepburn <kareem@hepburn.info>
2025-05-29 14:30:02 -0500

    feat(nvim): configure additional plugins

--- private_dot_config/exact_nvim/exact_lua/exact_config/exact_plugins/package-info.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_config/exact_plugins/package-info.lua
@@ -1,7 +1,5 @@
 return {
-  {
-    'vuki656/package-info.nvim',
-    dependencies = { 'MunifTanjim/nui.nvim' },
-    config = function() require('package-info').setup() end,
-  },
+  'vuki656/package-info.nvim',
+  dependencies = { 'MunifTanjim/nui.nvim' },
+  config = function() require('package-info').setup() end,
 }
