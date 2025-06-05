c7e63e8e8ea716dfe9bd771d4ada61fa370068a5
KH (neumachen) <kareem@hepburn.info>
2025-06-04 23:54:54 -0500

    refactor: revamp lazy.nvim setup

---
+++ private_dot_config/exact_nvim/exact_lua/exact_utils/ext.lua
@@ -0,0 +1,22 @@
+local M = {}
+--- Call the given function and use `vim.notify` to notify of any errors
+--- this function is a wrapper around `xpcall` which allows having a single
+--- error handler for all errors
+---@param msg string
+---@param func function
+---@param ... any
+---@return boolean, any
+---@overload fun(func: function, ...): boolean, any
+function M.pcall(msg, func, ...)
+  local args = { ... }
+  if type(msg) == 'function' then
+    local arg = func --[[@as any]]
+    args, func, msg = { arg, unpack(args) }, msg, nil
+  end
+  return xpcall(func, function(err)
+    msg = debug.traceback(msg and fmt('%s:\n%s\n%s', msg, vim.inspect(args), err) or err)
+    vim.schedule(function() vim.notify(msg, l.ERROR, { title = 'ERROR' }) end)
+  end, unpack(args))
+end
+
+return M
