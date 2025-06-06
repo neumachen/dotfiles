3f38ebd9a47c35cc318330d41beaac4c0c92c0fe
Kareem Hepburn <kareem@hepburn.info>
2025-06-06 01:52:45 -0500

    feat(lualine): Enhance statusline with macro recording and improved filename display

--- private_dot_config/exact_nvim/exact_lua/exact_plugins/lualine.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/lualine.lua
@@ -1,7 +1,10 @@
 return {
   'nvim-lualine/lualine.nvim',
   event = 'VeryLazy',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+    'yavorski/lualine-macro-recording.nvim',
+  },
   init = function()
     -- disable until lualine loads
     vim.opt.laststatus = 0
@@ -28,9 +31,22 @@ return {
         },
       },
       sections = {
-        lualine_a = { 'mode' },
+        lualine_a = { 'mode', { 'macro_recording', '%S' } },
         lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = { 'filename' },
+        lualine_c = {
+          {
+            'filename',
+            file_status = true,
+            newfile_status = true,
+            path = 4,
+            symbols = {
+              modified = '',
+              readonly = '',
+              unnamed = '',
+              newfile = '',
+            },
+          },
+        },
         lualine_x = { 'encoding', 'fileformat', 'filetype' },
         lualine_y = { 'progress' },
         lualine_z = { 'location' },
@@ -46,7 +62,13 @@ return {
       tabline = {},
       winbar = {},
       inactive_winbar = {},
-      extensions = {},
+      extensions = {
+        'fzf',
+        'lazy',
+        'mason',
+        'quickfix',
+        'trouble',
+      },
     })
   end,
 }
