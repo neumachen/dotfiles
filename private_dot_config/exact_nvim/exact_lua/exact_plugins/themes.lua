2c77f1f548be0cf28de1ca91a423b1a9bb38dae4
KH (neumachen) <kareem@hepburn.info>
2025-06-05 23:45:31 -0500

    chore(nvim): remove extra colorschemes

--- private_dot_config/exact_nvim/exact_lua/exact_plugins/themes.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/themes.lua
@@ -3,29 +3,19 @@ return {
     'catppuccin/nvim',
     name = 'catppuccin',
     priority = 1000,
+    opt = {
+      term_colors = true,
+    },
   },
   {
     'folke/tokyonight.nvim',
     priority = 1000,
   },
   {
-    'rebelot/kanagawa.nvim',
-    priority = 1000,
-  },
-  {
-    'EdenEast/nightfox.nvim',
-    priority = 1000,
-  },
-  {
-    'navarasu/onedark.nvim',
-    priority = 1000,
-  },
-  {
     'olimorris/onedarkpro.nvim',
     priority = 1000,
-  },
-  {
-    'tiagovla/tokyodark.nvim',
-    priority = 1000,
+    options = {
+      terminal_colors = true,
+    },
   },
 }
