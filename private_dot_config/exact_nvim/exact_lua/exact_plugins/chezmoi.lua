bd334fa8e9362dc11ba15e2db3915135294c1523
KH (neumachen) <kareem@hepburn.info>
2025-06-14 13:34:09 -0500

    feat(nvim): Refactor chezmoi plugin configuration with more granular event notifications

--- private_dot_config/exact_nvim/exact_lua/exact_plugins/chezmoi.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/chezmoi.lua
@@ -12,10 +12,28 @@ return {
           watch = true,
           force = true,
         },
-        notification = {
-          on_open = true,
-          on_apply = true,
-          on_watch = true,
+        events = {
+          on_open = {
+            notification = {
+              enable = true,
+              msg = 'Opened a chezmoi-managed file',
+              opts = {},
+            },
+          },
+          on_watch = {
+            notification = {
+              enable = true,
+              msg = 'This file will be automatically applied',
+              opts = {},
+            },
+          },
+          on_apply = {
+            notification = {
+              enable = true,
+              msg = 'Successfully applied',
+              opts = {},
+            },
+          },
         },
         telescope = {
           select = { '<CR>' },
