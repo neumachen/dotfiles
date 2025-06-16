932cb68641f878598069bc14df08dbf246fec00e
Kareem Hepburn <kareem@hepburn.info>
2025-06-16 10:33:02 -0500

    chore(config): Migrate and consolidate CSpell configuration files

--- private_dot_config/exact_nvim/exact_lsp/yamlls.lua
+++ private_dot_config/exact_nvim/exact_lsp/yamlls.lua
@@ -1,7 +1,10 @@
+local Lsp = require('utils.lsp')
+
 ---@diagnostic disable: missing-fields, inject-field
 ---@type vim.lsp.Client
 return {
   before_init = function(_, client_config) client_config.settings.yaml.schemas = require('schemastore').yaml.schemas() end,
+  on_attach = Lsp.on_attach,
   settings = {
     redhat = { telemetry = { enabled = false } },
     yaml = {
