4cbb3f8db3f67f25a00188457b21c2ec5ec0587c
Kareem Hepburn <kareem@hepburn.info>
2025-06-19 18:20:55 -0500

    feat(nvim): Enhance completion sources and configuration

--- private_dot_config/exact_nvim/exact_lua/exact_plugins/blink.cmp.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/blink.cmp.lua
@@ -1,7 +1,9 @@
--- Reference: https://github.com/linkarzu/dotfiles-latest/blob/main/neovim/neobean/lua/plugins/blink-cmp.lua
+-- reference: https://github.com/linkarzu/dotfiles-latest/blob/main/neovim/neobean/lua/plugins/blink-cmp.lua
 
 local trigger_text = ';'
 
+---@module 'lazy'
+---@type LazySpec
 return {
   'saghen/blink.cmp',
   event = 'InsertEnter',
@@ -14,6 +16,7 @@ return {
     'moyiz/blink-emoji.nvim',
     'Kaiser-Yang/blink-cmp-dictionary',
     'ribru17/blink-cmp-spell',
+    'mikavilpas/blink-ripgrep.nvim',
   },
   opts = function(_, opts)
     -- I noticed that telescope was extremely slow and taking too long to open,
@@ -24,9 +27,7 @@ return {
       -- Get the current buffer's filetype
       local filetype = vim.bo[0].filetype
       -- Disable for Telescope buffers
-      if filetype == 'TelescopePrompt' or filetype == 'minifiles' or filetype == 'snacks_picker_input' then
-        return false
-      end
+      if filetype == 'TelescopePrompt' or filetype == 'snacks_picker_input' then return false end
       return true
     end
 
@@ -34,7 +35,16 @@ return {
     -- Merge custom sources with the existing ones from lazyvim
     -- NOTE: by default lazyvim already includes the lazydev source, so not adding it here again
     opts.sources = vim.tbl_deep_extend('force', opts.sources or {}, {
-      default = { 'lsp', 'path', 'snippets', 'buffer', 'dadbod', 'emoji', 'spell', 'dictionary' },
+      default = {
+        'lsp',
+        'path',
+        'snippets',
+        'buffer',
+        'dadbod',
+        'emoji',
+        'spell',
+        'dictionary',
+      },
       providers = {
         lsp = {
           name = 'lsp',
@@ -138,7 +148,6 @@ return {
           min_keyword_length = 2,
           opts = { insert = true }, -- Insert emoji (default) or complete its name
         },
-
         -- https://github.com/Kaiser-Yang/blink-cmp-dictionary
         -- In macOS to get started with a dictionary:
         -- cp /usr/share/dict/words ~/github/dotfiles-latest/dictionaries/words.txt
@@ -201,17 +210,137 @@ return {
             end,
           },
         },
-        -- -- Third class citizen mf always talking shit
-        -- copilot = {
-        --   name = "copilot",
-        --   enabled = true,
-        --   module = "blink-cmp-copilot",
-        --   kind = "Copilot",
-        --   min_keyword_length = 6,
-        --   score_offset = -100, -- the higher the number, the higher the priority
-        --   async = true,
-        -- },
+        ripgrep = {
+          module = 'blink-ripgrep',
+          name = 'Ripgrep',
+          -- the options below are optional, some default values are shown
+          ---@module "blink-ripgrep"
+          ---@type blink-ripgrep.Options
+          opts = {
+            -- For many options, see `rg --help` for an exact description of
+            -- the values that ripgrep expects.
+
+            -- the minimum length of the current word to start searching
+            -- (if the word is shorter than this, the search will not start)
+            prefix_min_len = 2,
+
+            -- The number of lines to show around each match in the preview
+            -- (documentation) window. For example, 5 means to show 5 lines
+            -- before, then the match, and another 5 lines after the match.
+            context_size = 5,
+
+            -- The maximum file size of a file that ripgrep should include in
+            -- its search. Useful when your project contains large files that
+            -- might cause performance issues.
+            -- Examples:
+            -- "1024" (bytes by default), "200K", "1M", "1G", which will
+            -- exclude files larger than that size.
+            max_filesize = '1M',
+
+            -- Specifies how to find the root of the project where the ripgrep
+            -- search will start from. Accepts the same options as the marker
+            -- given to `:h vim.fs.root()` which offers many possibilities for
+            -- configuration. If none can be found, defaults to Neovim's cwd.
+            --
+            -- Examples:
+            -- - ".git" (default)
+            -- - { ".git", "package.json", ".root" }
+            project_root_marker = '.git',
+
+            -- Enable fallback to neovim cwd if project_root_marker is not
+            -- found. Default: `true`, which means to use the cwd.
+            project_root_fallback = true,
+
+            -- The casing to use for the search in a format that ripgrep
+            -- accepts. Defaults to "--ignore-case". See `rg --help` for all the
+            -- available options ripgrep supports, but you can try
+            -- "--case-sensitive" or "--smart-case".
+            search_casing = '--ignore-case',
+
+            -- (advanced) Any additional options you want to give to ripgrep.
+            -- See `rg -h` for a list of all available options. Might be
+            -- helpful in adjusting performance in specific situations.
+            -- If you have an idea for a default, please open an issue!
+            --
+            -- Not everything will work (obviously).
+            additional_rg_options = {},
+
+            -- When a result is found for a file whose filetype does not have a
+            -- treesitter parser installed, fall back to regex based highlighting
+            -- that is bundled in Neovim.
+            fallback_to_regex_highlighting = true,
+
+            -- Absolute root paths where the rg command will not be executed.
+            -- Usually you want to exclude paths using gitignore files or
+            -- ripgrep specific ignore files, but this can be used to only
+            -- ignore the paths in blink-ripgrep.nvim, maintaining the ability
+            -- to use ripgrep for those paths on the command line. If you need
+            -- to find out where the searches are executed, enable `debug` and
+            -- look at `:messages`.
+            ignore_paths = {},
+
+            -- Any additional paths to search in, in addition to the project
+            -- root. This can be useful if you want to include dictionary files
+            -- (/usr/share/dict/words), framework documentation, or any other
+            -- reference material that is not available within the project
+            -- root.
+            additional_paths = {},
+
+            -- Keymaps to toggle features on/off. This can be used to alter
+            -- the behavior of the plugin without restarting Neovim. Nothing
+            -- is enabled by default. Requires folke/snacks.nvim.
+            toggles = {
+              -- The keymap to toggle the plugin on and off from blink
+              -- completion results. Example: "<leader>tg" ("toggle grep")
+              on_off = nil,
+
+              -- The keymap to toggle debug mode on/off. Example: "<leader>td" ("toggle debug")
+              debug = nil,
+            },
+
+            -- Features that are not yet stable and might change in the future.
+            -- You can enable these to try them out beforehand, but be aware
+            -- that they might change. Nothing is enabled by default.
+            future_features = {
+              backend = {
+                -- The backend to use for searching. Defaults to "ripgrep".
+                -- Available options:
+                -- - "ripgrep", always use ripgrep
+                -- - "gitgrep", always use git grep
+                -- - "gitgrep-or-ripgrep", use git grep if possible, otherwise
+                --   ripgrep
+                use = 'ripgrep',
+              },
+            },
+
+            -- Show debug information in `:messages` that can help in
+            -- diagnosing issues with the plugin.
+            debug = false,
+          },
+          -- (optional) customize how the results are displayed. Many options
+          -- are available - make sure your lua LSP is set up so you get
+          -- autocompletion help
+          transform_items = function(_, items)
+            for _, item in ipairs(items) do
+              -- example: append a description to easily distinguish rg results
+              item.labelDetails = {
+                description = '(rg)',
+              }
+            end
+            return items
+          end,
+        },
       },
+      -- -- Third class citizen mf always talking shit
+      -- copilot = {
+      --   name = "copilot",
+      --   enabled = true,
+      --   module = "blink-cmp-copilot",
+      --   kind = "Copilot",
+      --   min_keyword_length = 6,
+      --   score_offset = -100, -- the higher the number, the higher the priority
+      --   async = true,
+      -- },
     })
 
     opts.cmdline = {
