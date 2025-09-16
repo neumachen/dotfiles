3a44544514be68635b04b2a240a88cd10f65215e
Kareem Hepburn <kareem@hepburn.info>
2025-09-16 15:30:29 -0500

    feat(lsp): Configure YAML language server with enhanced settings and filetypes

--- private_dot_config/exact_nvim/exact_lsp/yamlls.lua
+++ private_dot_config/exact_nvim/exact_lsp/yamlls.lua
@@ -4,6 +4,9 @@ local Lsp = require('utils.lsp')
 ---@type vim.lsp.Client
 return {
   before_init = function(_, client_config) client_config.settings.yaml.schemas = require('schemastore').yaml.schemas() end,
+  cmd = { 'yaml-language-server', '--stdio' },
+  filetypes = { 'yaml', 'yaml.docker-compose', 'yaml.gitlab', 'yaml.helm-values' },
+  root_markers = { '.git' },
   on_attach = Lsp.on_attach,
   settings = {
     redhat = { telemetry = { enabled = false } },
