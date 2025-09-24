6d3ab45032deb308fbd9b278229b85e3781714db
KH (neumachen) <kareem@hepburn.info>
2025-09-24 10:45:25 -0500

    feat(lsp): Add dynamic LSP server configuration and update Neovim setup

--- private_dot_config/exact_nvim/exact_lsp/lua_ls.lua
+++ private_dot_config/exact_nvim/exact_lsp/lua_ls.lua
@@ -1,9 +1,9 @@
-local Lsp = require "utils.lsp"
+local Lsp = require('utils.lsp')
 -- NOTE: mise use -g lua-language-server
 -- Credit to https://lsp-zero.netlify.app/blog/lsp-config-overview.html
 return {
-  cmd = { "lua-language-server" },
+  cmd = { 'lua-language-server' },
   on_attach = Lsp.on_attach,
-  filetypes = { "lua" },
-  root_markers = { ".luarc.json", ".luarc.jsonc" },
+  filetypes = { 'lua' },
+  root_markers = { '.luarc.json', '.luarc.jsonc' },
 }
