bdc65b692c2b69f318ba83b8caefe94df11ca910
KH (neumachen) <kareem@hepburn.info>
2025-09-24 10:45:25 -0500

    feat(lsp): Add dynamic LSP server configuration and update Neovim setup

---
+++ private_dot_config/exact_nvim/exact_lsp/docker_language_server.lua
@@ -0,0 +1,18 @@
+local Lsp = require('utils.lsp')
+-- mise install -g github:docker/docker-language-server
+return {
+  cmd = { 'docker-language-server', 'start', '--stdio' },
+  on_attach = Lsp.on_attach,
+  filetypes = { 'dockerfile', 'yaml.docker-compose' },
+  root_markers = {
+    'Dockerfile',
+    'docker-compose.yaml',
+    'docker-compose.yml',
+    'compose.yaml',
+    'compose.yml',
+    'docker-bake.json',
+    'docker-bake.hcl',
+    'docker-bake.override.json',
+    'docker-bake.override.hcl',
+  },
+}
--- private_dot_config/exact_nvim/exact_lua/exact_plugins/init.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/init.lua
@@ -2,26 +2,7 @@ return {
   'nvim-lua/plenary.nvim', -- THE LIBRARY
   'nvim-tree/nvim-web-devicons',
   'echasnovski/mini.icons',
-  {
-    'williamboman/mason.nvim',
-    cmd = { 'Mason', 'MasonUpdate', 'MasonInstall', 'MasonLog' },
-    keys = { { '<leader>cm', '<cmd>Mason<cr>', desc = 'Mason' } },
-    build = ':MasonUpdate',
-    ---@param opts MasonSettings | {ensure_installed: string[]}
-    config = function(_, opts)
-      require('mason').setup(opts)
-      local mr = require('mason-registry')
-      mr:on('package:install:success', function()
-        vim.defer_fn(function()
-          -- trigger FileType event to possibly load this newly installed LSP server
-          require('lazy.core.handler.event').trigger({
-            event = 'FileType',
-            buf = vim.api.nvim_get_current_buf(),
-          })
-        end, 100)
-      end)
-    end,
-  },
+  'neovim/nvim-lspconfig',
   {
     'folke/lazydev.nvim',
     ft = 'lua',
