0bf4914d0864a7930846be7a66e20eaac2207673
KH (neumachen) <kareem@hepburn.info>
2025-11-23 22:54:09 +0000

    feat(nvim): add jq-playground plugin configuration
    Add jq-playground.nvim plugin with custom window layout settings for interactive jq query testing. Configures output window to split right with JSON filetype and query editor to split below at 30% height.

---
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/jq-playground.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    'yochem/jq-playground.nvim',
+    config = function()
+      require('jq-playground').setup({
+        cmd = { 'jq' },
+        output_window = {
+          split_direction = 'right',
+          width = nil,
+          height = nil,
+          scratch = true,
+          filetype = 'json',
+          name = 'jq output',
+        },
+        query_window = {
+          split_direction = 'below',
+          width = nil,
+          height = 0.3,
+          scratch = false,
+          filetype = 'jq',
+          name = 'jq query editor',
+        },
+        disable_default_keymap = false,
+      })
+    end,
+  },
+}
