d5e431feb4d9215591fec901c083eae9621b1b1a
KH (neumachen) <kareem@hepburn.info>
2025-11-27 02:51:42 +0000

    refactor(nvim): extract tokyonight theme config to separate module

---
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/themes/tokyonight.lua
@@ -0,0 +1,48 @@
+return {
+  style = 'storm',
+  light_style = 'day',
+  transparent = false,
+  terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+  styles = {
+    -- Style to be applied to different syntax groups
+    -- Value is any valid attr-list value for `:help nvim_set_hl`
+    comments = { italic = true },
+    keywords = { italic = true },
+    functions = {},
+    variables = {},
+    -- Background styles. Can be "dark", "transparent" or "normal"
+    sidebars = 'dark', -- style for sidebars, see below
+    floats = 'dark', -- style for floating windows
+  },
+  day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+  dim_inactive = false, -- dims inactive windows
+  lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold
+
+  --- You can override specific color groups to use other groups or a hex color
+  --- function will be called with a ColorScheme table
+  ---@param colors table
+  on_colors = function(colors) end,
+
+  --- You can override specific highlights to use other groups or a hex color
+  --- function will be called with a Highlights and ColorScheme table
+  ---@param highlights table
+  ---@param colors table
+  on_highlights = function(highlights, colors)
+    -- Make line numbers more visible with blue color that complements cyan
+    highlights.LineNrAbove = { fg = colors.purple }
+    highlights.LineNrBelow = { fg = colors.purple }
+    -- Make current line number even more visible with bright teal
+    highlights.LineNr = { fg = colors.orange, bold = true }
+  end,
+
+  cache = true,
+
+  ---@type table<string, boolean|{enabled:boolean}>
+  plugins = {
+    all = package.loaded.lazy == nil,
+    -- Uses your plugin manager to automatically enable needed plugins
+    -- currently only lazy.nvim is supported
+    auto = true,
+    telescope = true,
+  },
+}
