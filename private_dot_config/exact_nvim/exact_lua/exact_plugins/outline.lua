96bbe9614a67780aec2bba19c88e5fe1eeae8131
KH (neumachen) <kareem@hepburn.info>
2025-11-27 01:48:37 +0000

    refactor(nvim): change keybindings from leader to localleader for toggle commands

--- private_dot_config/exact_nvim/exact_lua/exact_plugins/outline.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/outline.lua
@@ -3,7 +3,7 @@ return {
     'hedyhli/outline.nvim',
     config = function()
       -- Example mapping to toggle outline
-      vim.keymap.set('n', '<leader>cso', '<cmd>Outline<CR>', { desc = 'Toggle Outline' })
+      vim.keymap.set('n', '<localleader>to', '<cmd>Outline<CR>', { desc = 'Toggle Outline' })
 
       require('outline').setup({
         outline_window = {
