efb3180c3e87acf953af8f596a43abc0fd3d28ad
KH (neumachen) <kareem@hepburn.info>
2025-11-29 05:03:13 +0000

    refactor(nvim,wezterm)!: change keybindings for window navigation and line movement
    Replace Alt with Ctrl+Alt for line movement in Neovim to avoid conflicts with smart-splits window navigation. Update smart-splits plugin configuration to use Ctrl for window navigation and Alt for resizing. Migrate WezTerm from smart-splits plugin to manual implementation with consistent keybindings. Remove redundant window navigation and resize mappings from keymaps.lua.
    
    BREAKING CHANGE: Line movement keybindings changed from Alt+j/k to Ctrl+Alt+j/k in all modes. Window navigation now uses Ctrl+h/j/k/l and window resizing uses Alt+h/j/k/l.

--- private_dot_config/exact_nvim/exact_lua/exact_config/keymaps.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_config/keymaps.lua
@@ -9,35 +9,30 @@ map({ 'n', 'x' }, '<Down>', "v:count == 0 ? 'gj' : 'j'", { desc = 'Down', expr =
 map({ 'n', 'x' }, 'k', "v:count == 0 ? 'gk' : 'k'", { desc = 'Up', expr = true, silent = true })
 map({ 'n', 'x' }, '<Up>', "v:count == 0 ? 'gk' : 'k'", { desc = 'Up', expr = true, silent = true })
 
--- Go to different windows
-map('n', '<C-h>', '<C-w>h', { desc = 'Go to Left Window', remap = true })
-map('n', '<C-j>', '<C-w>j', { desc = 'Go to Lower Window', remap = true })
-map('n', '<C-k>', '<C-w>k', { desc = 'Go to Upper Window', remap = true })
-map('n', '<C-l>', '<C-w>l', { desc = 'Go to Right Window', remap = true })
-
--- Resize window using <ctrl> arrow keys
-map('n', '<C-Up>', '<cmd>resize +2<cr>', { desc = 'Increase Window Height' })
-map('n', '<C-Down>', '<cmd>resize -2<cr>', { desc = 'Decrease Window Height' })
-map('n', '<C-Left>', '<cmd>vertical resize -2<cr>', { desc = 'Decrease Window Width' })
-map('n', '<C-Right>', '<cmd>vertical resize +2<cr>', { desc = 'Increase Window Width' })
-
 -- Move Lines
-map('n', '<A-j>', "<cmd>execute 'move .+' . v:count1<cr>==", { desc = 'Move Down' })
-map('n', '<A-k>', "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = 'Move Up' })
-map('i', '<A-j>', '<esc><cmd>m .+1<cr>==gi', { desc = 'Move Down' })
-map('i', '<A-k>', '<esc><cmd>m .-2<cr>==gi', { desc = 'Move Up' })
-map('v', '<A-j>', ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = 'Move Down' })
-map('v', '<A-k>', ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = 'Move Up' })
+local opts = { noremap = true, silent = true }
+
+-- NORMAL mode: move current line
+map('n', '<C-A-j>', ':m .+1<CR>==', vim.tbl_extend('force', opts, { desc = 'Move line down' }))
+map('n', '<C-A-k>', ':m .-2<CR>==', vim.tbl_extend('force', opts, { desc = 'Move line up' }))
+
+-- INSERT mode: move current line, return to insert at same spot
+map('i', '<C-A-j>', '<Esc>:m .+1<CR>==gi', vim.tbl_extend('force', opts, { desc = 'Move line down' }))
+map('i', '<C-A-k>', '<Esc>:m .-2<CR>==gi', vim.tbl_extend('force', opts, { desc = 'Move line up' }))
+
+-- VISUAL mode: move selected lines as a block
+map('v', '<C-A-j>', ":m '>+1<CR>gv=gv", vim.tbl_extend('force', opts, { desc = 'Move block down' }))
+map('v', '<C-A-k>', ":m '<-2<CR>gv=gv", vim.tbl_extend('force', opts, { desc = 'Move block up' }))
 
 -- Goto
 map('n', 'gl', '$', { desc = 'Go to end of line' })
-map('n', 'gh', '^', { desc = 'Go to start of line' })
+map('n', 'gh', '^', { desc = 'go to start of line' })
 
 -- buffers
-map('n', '<S-h>', '<cmd>bprevious<cr>', { desc = 'Prev Buffer' })
-map('n', '<S-l>', '<cmd>bnext<cr>', { desc = 'Next Buffer' })
-map('n', '[b', '<cmd>bprevious<cr>', { desc = 'Prev Buffer' })
-map('n', ']b', '<cmd>bnext<cr>', { desc = 'Next Buffer' })
+map('n', '<s-h>', '<cmd>bprevious<cr>', { desc = 'prev buffer' })
+map('n', '<s-l>', '<cmd>bnext<cr>', { desc = 'next buffer' })
+map('n', '[b', '<cmd>bprevious<cr>', { desc = 'prev buffer' })
+map('n', ']b', '<cmd>bnext<cr>', { desc = 'next buffer' })
 map('n', '<leader>bb', '<cmd>e #<cr>', { desc = 'Switch to Other Buffer' })
 map('n', '<leader>`', '<cmd>e #<cr>', { desc = 'Switch to Other Buffer' })
 
@@ -244,15 +239,6 @@ map('n', '<A-l>', '$', {
   desc = 'Go to end of line',
   silent = true,
 })
-
--- Move live up or down
--- moving
-map('n', '<A-Down>', ':m .+1<CR>', opts)
-map('n', '<A-Up>', ':m .-2<CR>', opts)
-map('i', '<A-Down>', '<Esc>:m .+1<CR>==gi', opts)
-map('i', '<A-Up>', '<Esc>:m .-2<CR>==gi', opts)
-map('v', '<A-Down>', ":m '>+1<CR>gv=gv", opts)
-map('v', '<A-Up>', ":m '<-2<CR>gv=gv", opts)
 
 -- Fix Spell checking
 map('n', 'z0', '1z=', {
