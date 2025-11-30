d29cb3345e830338767a00d1f777217d8a8b5a85
KH (neumachen) <kareem@hepburn.info>
2025-11-30 03:30:37 +0000

    feat(nvim): replace blink-cmp-dictionary with blink-cmp-words and enhance lazydev config
    Replace Kaiser-Yang/blink-cmp-dictionary with archie-judd/blink-cmp-words for improved dictionary and thesaurus support. Add lazydev integration to blink.cmp with higher priority scoring. Enhance lazydev configuration with wezterm-types dependency and additional library paths. Apply code formatting improvements throughout blink.cmp.lua.

--- private_dot_config/exact_nvim/exact_lua/exact_plugins/init.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/init.lua
@@ -5,7 +5,28 @@ return {
   'neovim/nvim-lspconfig',
   {
     'folke/lazydev.nvim',
-    ft = 'lua',
-    opts = { library = { { path = 'luvit-meta/library', words = { 'vim%.uv' } } } },
+    ft = 'lua', -- only load on lua files
+    dependencies = {
+      {
+        'DrKJeff16/wezterm-types',
+        lazy = true,
+        version = false, -- Get the latest version
+      },
+    },
+    opts = {
+      library = {
+        'lazy.nvim',
+        -- It can also be a table with trigger words / mods
+        -- Only load luvit types when the `vim.uv` word is found
+        { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+        { path = 'wezterm-types', mods = { 'wezterm' } },
+      },
+      -- Always enable unless `vim.g.lazydev_enabled = false`
+      -- This is the default
+      ---@diagnostic disable-next-line: unused-local
+      enabled = function(root_dir)
+        return vim.g.lazydev_enabled == nil and true or vim.g.lazydev_enabled
+      end,
+    },
   },
 }
