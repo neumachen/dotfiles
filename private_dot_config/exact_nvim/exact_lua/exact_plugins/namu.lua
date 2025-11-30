4c8ec901d35a917acbb146fa299d190b52e2f77a
KH (neumachen) <kareem@hepburn.info>
2025-11-30 14:42:43 +0000

    feat(nvim): add namu.nvim plugin for symbol navigation
    Add namu.nvim plugin configuration with comprehensive symbol navigation
    capabilities including buffer symbols, workspace symbols, watchtower,
    diagnostics, ctags, and help symbols. Configure language-specific symbol
    kinds, blocklists, and custom keymaps for integration with CodeCompanion
    and Avante. Add which-key group for Namu operations under <localleader>tn.

---
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/namu.lua
@@ -0,0 +1,227 @@
+return {
+  'bassamsdata/namu.nvim',
+  lazy = false,
+  config = function()
+    require('namu').setup({
+      -- Enable symbols navigator which is the default
+      namu_symbols = {
+        enable = true,
+        ---@type NamuConfig
+        options = {
+          AllowKinds = {
+            default = {
+              'Function',
+              'Method',
+              'Class',
+              'Module',
+              'Property',
+              'Variable',
+              'Constant',
+              'Enum',
+              'Interface',
+              'Field',
+              'Struct',
+            },
+            go = {
+              'Function',
+              'Method',
+              'Struct',
+              'Field',
+              'Interface',
+              'Constant',
+              'Variable',
+              'Property',
+              'TypeParameter',
+            },
+            lua = { 'Function', 'Method', 'Table', 'Module' },
+            python = { 'Function', 'Class', 'Method' },
+            yaml = { 'Object', 'Array' },
+            json = { 'Module' },
+            toml = { 'Object' },
+            markdown = { 'String' },
+          },
+          BlockList = {
+            default = {},
+            lua = {
+              '^vim%.', -- anonymous functions passed to nvim api
+              '%.%.%. :', -- vim.iter functions
+              ':gsub', -- lua string.gsub
+              '^callback$', -- nvim autocmds
+              '^filter$',
+              '^map$', -- nvim keymaps
+            },
+            python = { '^__' }, -- ignore __init__ functions
+          },
+          display = {
+            mode = 'icon',
+            padding = 2,
+          },
+          row_position = 'top10',
+          preview = {
+            highlight_on_move = true,
+            highlight_mode = 'always',
+          },
+          window = {
+            auto_size = true,
+            min_height = 1,
+            min_width = 20,
+            max_width = 120,
+            max_height = 30,
+            padding = 2,
+            border = 'rounded',
+            title_pos = 'left',
+            show_footer = true,
+            footer_pos = 'right',
+            relative = 'editor',
+            style = 'minimal',
+            width_ratio = 0.6,
+            height_ratio = 0.6,
+            title_prefix = '󱠦 ',
+          },
+          debug = false,
+          focus_current_symbol = true,
+          auto_select = false,
+          initially_hidden = false,
+          multiselect = {
+            enabled = true,
+            indicator = '✓',
+            keymaps = {
+              toggle = '<Tab>',
+              untoggle = '<S-Tab>',
+              select_all = '<C-a>',
+              clear_all = '<C-l>',
+            },
+            max_items = nil,
+          },
+          actions = {
+            close_on_yank = false,
+            close_on_delete = true,
+          },
+          movement = {
+            next = { '<C-n>', '<DOWN>' },
+            previous = { '<C-p>', '<UP>' },
+            close = { '<ESC>', '<C-c>', 'q' },
+            select = { '<CR>' },
+            delete_word = {}, -- it can assign "<C-w>"
+            clear_line = {}, -- it can be "<C-u>"
+          },
+          custom_keymaps = {
+            yank = {
+              keys = { '<C-y>' },
+              desc = 'Yank symbol text',
+            },
+            delete = {
+              keys = { '<C-d>' },
+              desc = 'Delete symbol text',
+            },
+            vertical_split = {
+              keys = { '<C-v>' },
+              desc = 'Open in vertical split',
+            },
+            horizontal_split = {
+              keys = { '<C-h>' },
+              desc = 'Open in horizontal split',
+            },
+            codecompanion = {
+              keys = '<C-o>',
+              desc = 'Add symbol to CodeCompanion',
+            },
+            avante = {
+              keys = '<C-t>',
+              desc = 'Add symbol to Avante',
+            },
+          },
+          icon = '󱠦',
+          kindText = {
+            Function = 'function',
+            Class = 'class',
+            Module = 'module',
+            Constructor = 'constructor',
+            Interface = 'interface',
+            Property = 'property',
+            Field = 'field',
+            Enum = 'enum',
+            Constant = 'constant',
+            Variable = 'variable',
+          },
+          kindIcons = {
+            File = '󰈙',
+            Module = '󰏗',
+            Namespace = '󰌗',
+            Package = '󰏖',
+            Class = '󰌗',
+            Method = '󰆧',
+            Property = '󰜢',
+            Field = '󰜢',
+            Constructor = '󰆧',
+            Enum = '󰒻',
+            Interface = '󰕘',
+            Function = '󰊕',
+            Variable = '󰀫',
+            Constant = '󰏿',
+            String = '󰀬',
+            Number = '󰎠',
+            Boolean = '󰨙',
+            Array = '󰅪',
+            Object = '󰅩',
+            Key = '󰌋',
+            Null = '󰟢',
+            EnumMember = '󰒻',
+            Struct = '󰌗',
+            Event = '󰉁',
+            Operator = '󰆕',
+            TypeParameter = '󰊄',
+          },
+          highlight = 'NamuPreview',
+          highlights = {
+            parent = 'NamuParent',
+            nested = 'NamuNested',
+            style = 'NamuStyle',
+          },
+          kinds = {
+            prefix_kind_colors = true,
+            enable_highlights = true,
+            highlights = {
+              PrefixSymbol = 'NamuPrefixSymbol',
+              Function = 'NamuSymbolFunction',
+              Method = 'NamuSymbolMethod',
+              Class = 'NamuSymbolClass',
+              Interface = 'NamuSymbolInterface',
+              Variable = 'NamuSymbolVariable',
+              Constant = 'NamuSymbolConstant',
+              Property = 'NamuSymbolProperty',
+              Field = 'NamuSymbolField',
+              Enum = 'NamuSymbolEnum',
+              Module = 'NamuSymbolModule',
+            },
+          },
+        },
+      },
+    })
+
+    vim.keymap.set('n', '<localleader>tns', ':Namu symbols<cr>', {
+      desc = 'Namu buffer symbols',
+      silent = true,
+    })
+    vim.keymap.set('n', '<localleader>tnw', ':Namu workspace<cr>', {
+      desc = 'Namu workspace symbols',
+      silent = true,
+    })
+    vim.keymap.set('n', '<localleader>tnW', ':Namu watchtower<cr>', {
+      desc = 'Namu watchtower symbols',
+      silent = true,
+    })
+    vim.keymap.set('n', '<localleader>tnd', ':Namu diagnostics<cr>', {
+      desc = 'Namu diagnostics symbols',
+      silent = true,
+    })
+    vim.keymap.set('n', '<localleader>tnc', ':Namu ctags<cr>', {
+      desc = 'Namu ctags symbols',
+      silent = true,
+    })
+    vim.keymap.set('n', '<localleader>tnh', ':Namu help<cr>', {
+      desc = 'Namu help symbols',
+      silent = true,
+    })
+  end,
+}
