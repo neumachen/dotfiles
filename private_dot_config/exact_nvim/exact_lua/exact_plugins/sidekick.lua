7a20b85256a421e25d5a19b3f2ad4f61dd149ce7
KH (neumachen) <kareem@hepburn.info>
2025-11-30 01:55:50 +0000

    refactor(nvim): remove sidekick.nvim CLI multiplexer configuration
    Remove zellij backend configuration from sidekick.nvim plugin opts, simplifying to empty opts table.

--- private_dot_config/exact_nvim/exact_lua/exact_plugins/sidekick.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/sidekick.lua
@@ -1,14 +1,6 @@
 return {
   'folke/sidekick.nvim',
-  opts = {
-    -- add any options here
-    cli = {
-      mux = {
-        backend = 'zellij',
-        enabled = true,
-      },
-    },
-  },
+  opts = {},
   -- stylua: ignore
   keys = {
     {
