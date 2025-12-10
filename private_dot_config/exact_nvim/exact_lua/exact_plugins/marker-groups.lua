3f42bbccea137babf04e3d1a6685d35ee421cbd9
Kareem Hepburn <kareem@hepburn.info>
2025-12-09 22:00:46 -0600

    refactor(nvim): Simplify Neovim plugin configuration and remove unused plugins

--- private_dot_config/exact_nvim/exact_lua/exact_plugins/marker-groups.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/marker-groups.lua
@@ -4,10 +4,6 @@ return {
     'nvim-lua/plenary.nvim', -- Required
     'ibhagwan/fzf-lua', -- Optional: fzf-lua picker
     'folke/snacks.nvim', -- Optional: Snacks picker
-    'nvim-telescope/telescope.nvim', -- Optional: Telescope picker
-    -- mini.pick is part of mini.nvim; this plugin vendors mini.nvim for tests,
-    -- but you can also install mini.nvim explicitly to use mini.pick system-wide
-    -- "nvim-mini/mini.nvim",
   },
   config = function()
     require('marker-groups').setup({
--- private_dot_config/exact_nvim/exact_lua/exact_plugins/trouble.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/trouble.lua
@@ -1,38 +1,5 @@
 return {
   'folke/trouble.nvim',
   event = 'VeryLazy',
-  opts = {}, -- for default options, refer to the configuration section for custom setup.
   cmd = 'Trouble',
-  keys = {
-    {
-      '<leader>xx',
-      '<cmd>Trouble diagnostics toggle<cr>',
-      desc = 'Diagnostics (Trouble)',
-    },
-    {
-      '<leader>xX',
-      '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
-      desc = 'Buffer Diagnostics (Trouble)',
-    },
-    {
-      '<localleader>ts',
-      '<cmd>Trouble symbols toggle focus=false<cr>',
-      desc = 'Toggle Symbols (Trouble)',
-    },
-    {
-      '<leader>cl',
-      '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
-      desc = 'LSP Definitions / references / ... (Trouble)',
-    },
-    {
-      '<leader>xL',
-      '<cmd>Trouble loclist toggle<cr>',
-      desc = 'Location List (Trouble)',
-    },
-    {
-      '<leader>xQ',
-      '<cmd>Trouble qflist toggle<cr>',
-      desc = 'Quickfix List (Trouble)',
-    },
-  },
 }
