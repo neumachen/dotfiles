de6ba02b1269bbcc41e8d44a270c0c5aa5bcce1d
Kareem Hepburn <kareem@hepburn.info>
2025-12-10 11:45:50 -0600

    refactor: update WezTerm keybindings and smart-splits plugin config

--- private_dot_config/exact_wezterm/plugins/smart-splits.lua
+++ private_dot_config/exact_wezterm/plugins/smart-splits.lua
@@ -1,6 +1,6 @@
 local w = require('wezterm')
 
--- if you are *NOT* lazy-loading smart-splits.nvim (recommended)
+-- if you are *NOT* lazy-loading smart-splits.nvim (recommended)ScrollByLine
 local function is_vim(pane)
   -- this is set by the plugin, and unset on ExitPre in Neovim
   return pane:get_user_vars().IS_NVIM == 'true'
@@ -21,13 +21,22 @@ local function split_nav(resize_or_move, key)
       if is_vim(pane) then
         -- pass the keys through to vim/nvim
         win:perform_action({
-          SendKey = { key = key, mods = resize_or_move == 'resize' and 'META' or 'CTRL' },
+          SendKey = {
+            key = key,
+            mods = resize_or_move == 'resize' and 'META' or 'CTRL',
+          },
         }, pane)
       else
         if resize_or_move == 'resize' then
-          win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+          win:perform_action(
+            { AdjustPaneSize = { direction_keys[key], 3 } },
+            pane
+          )
         else
-          win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+          win:perform_action(
+            { ActivatePaneDirection = direction_keys[key] },
+            pane
+          )
         end
       end
     end),
