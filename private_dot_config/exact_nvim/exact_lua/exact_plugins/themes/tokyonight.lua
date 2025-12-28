a9e6780f6d82c94cad4802a44e005309736188f1
Kareem Hepburn <kareem@hepburn.info>
2025-12-28 02:56:22 -0600

    style(theme): Disable custom theme configurations and enable transparency

--- private_dot_config/exact_nvim/exact_lua/exact_plugins/themes/tokyonight.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/themes/tokyonight.lua
@@ -22,41 +22,41 @@ end
 return {
   style = 'storm',
   light_style = 'day',
-  transparent = false,
-  terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
-  styles = {
-    -- Style to be applied to different syntax groups
-    -- Value is any valid attr-list value for `:help nvim_set_hl`
-    comments = { italic = true },
-    keywords = { italic = true },
-    functions = {},
-    variables = {},
-    -- Background styles. Can be "dark", "transparent" or "normal"
-    sidebars = 'dark', -- style for sidebars, see below
-    floats = 'dark', -- style for floating windows
-  },
+  transparent = true,
+  terminal_colors = false, -- Configure the colors used when opening a `:terminal` in Neovim
+  -- styles = {
+  --   -- Style to be applied to different syntax groups
+  --   -- Value is any valid attr-list value for `:help nvim_set_hl`
+  --   comments = { italic = true },
+  --   keywords = { italic = true },
+  --   functions = {},
+  --   variables = {},
+  --   -- Background styles. Can be "dark", "transparent" or "normal"
+  --   sidebars = 'dark', -- style for sidebars, see below
+  --   floats = 'dark', -- style for floating windows
+  -- },
   day_brightness = 0.3, -- Adjusts brightness of the colors of the Day style
   dim_inactive = false,
   lualine_bold = true,
 
-  on_colors = function(colors)
-    if is_light_background() then return end
+  -- on_colors = function(colors)
+  --   if is_light_background() then return end
+  --
+  --   colors.bg = BACKGROUND_DARK
+  --   colors.bg_dark = BACKGROUND_DARK
+  --   colors.bg_float = BACKGROUND_DARK
+  --   colors.bg_highlight = BACKGROUND_DARK
+  --   colors.bg_sidebar = BACKGROUND_DARK
+  --   colors.bg_statusline = BACKGROUND_DARK
+  -- end,
 
-    colors.bg = BACKGROUND_DARK
-    colors.bg_dark = BACKGROUND_DARK
-    colors.bg_float = BACKGROUND_DARK
-    colors.bg_highlight = BACKGROUND_DARK
-    colors.bg_sidebar = BACKGROUND_DARK
-    colors.bg_statusline = BACKGROUND_DARK
-  end,
+  -- on_highlights = function(highlights, colors)
+  --   if not is_light_background() then
+  --     set_background_colors(highlights, BACKGROUND_DARK)
+  --   end
+  -- end,
 
-  on_highlights = function(highlights, colors)
-    if not is_light_background() then
-      set_background_colors(highlights, BACKGROUND_DARK)
-    end
-  end,
-
-  cache = true,
+  -- cache = true,
 
   plugins = {
     all = package.loaded.lazy == nil,
