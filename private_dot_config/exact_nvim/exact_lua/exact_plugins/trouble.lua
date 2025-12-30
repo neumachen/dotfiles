e5d514a2bd62ca148680b956ddccb5a00057d38a
Kareem Hepburn <kareem@hepburn.info>
2025-12-30 14:32:51 -0600

    refactor(nvim): reorganize trouble.nvim keybindings to use localleader
    - Changed trouble.nvim keybindings from <leader>x* to <localleader>d* pattern for better ergonomics
    - Removed symbols and LSP definition bindings, keeping only core diagnostic functions
    - Added diagnostic group mapping in which-key for the new localleader pattern
    - Enabled config for nerdy.nvim plugin
    - Added "neovim" to spell-check dictionary
    
    The <leader>x bindings were difficult to press, so moved core diagnostic functions to <localleader>d (diagnostics) namespace for more comfortable access.

--- private_dot_config/exact_nvim/exact_lua/exact_plugins/trouble.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/trouble.lua
@@ -7,32 +7,22 @@ return {
   opts = {},
   keys = {
     {
-      '<leader>xx',
+      '<localleader>T',
       '<cmd>Trouble diagnostics toggle<cr>',
       desc = 'Diagnostics (Trouble)',
     },
     {
-      '<leader>xX',
+      '<localleader>db',
       '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
       desc = 'Buffer Diagnostics (Trouble)',
     },
     {
-      '<leader>cs',
-      '<cmd>Trouble symbols toggle focus=false<cr>',
-      desc = 'Symbols (Trouble)',
-    },
-    {
-      '<leader>cl',
-      '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
-      desc = 'LSP Definitions / references / ... (Trouble)',
-    },
-    {
-      '<leader>xL',
+      '<localleader>dL',
       '<cmd>Trouble loclist toggle<cr>',
       desc = 'Location List (Trouble)',
     },
     {
-      '<leader>xQ',
+      '<localleader>dq',
       '<cmd>Trouble qflist toggle<cr>',
       desc = 'Quickfix List (Trouble)',
     },
