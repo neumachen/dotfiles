12cd9eb0d8e5dc73db449f59cf94eee5f64cc21c
Kareem Hepburn <kareem@hepburn.info>
2025-12-31 11:34:04 -0600

    chore(dotfiles): add Treesitter predicate; change yazi sort order
    - Register a custom nvim-treesitter query predicate "is-mise?" that returns true
      for buffers whose filename matches ".*mise.*.toml" (added in the plugin init).
      This lets Treesitter queries target those specific TOML files.
    - Update yazi configuration to use sort_by = "extension" instead of "natural"
      to change the sorting behavior to be extension-based.
    
    Tests: not indicated.

--- private_dot_config/exact_nvim/exact_lua/exact_plugins/nvim-treesitter.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/nvim-treesitter.lua
@@ -13,6 +13,17 @@ return {
   },
   branch = 'main',
   build = ':TSUpdate',
+  init = function()
+    require('vim.treesitter.query').add_predicate(
+      'is-mise?',
+      function(_, _, bufnr, _)
+        local filepath = vim.api.nvim_buf_get_name(tonumber(bufnr) or 0)
+        local filename = vim.fn.fnamemodify(filepath, ':t')
+        return string.match(filename, '.*mise.*%.toml$') ~= nil
+      end,
+      { force = true, all = false }
+    )
+  end,
   config = function()
     local ts = require('nvim-treesitter')
 
