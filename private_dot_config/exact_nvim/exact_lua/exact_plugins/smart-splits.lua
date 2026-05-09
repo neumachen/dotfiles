56cb659c9a1e8ae0446b46fca1af37ec679da67f
Kareem Hepburn <kareem@hepburn.info>
2026-05-09 20:47:01 +0000

    refactor(nvim): use smart-splits for terminal window navigation
    - Moved <C-h/j/k/l> terminal mappings from keymaps.lua to smart-splits
    - Now uses smart-splits in both normal and terminal modes
    - Provides consistent behavior and tmux integration

--- private_dot_config/exact_nvim/exact_lua/exact_plugins/smart-splits.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/smart-splits.lua
@@ -30,11 +30,11 @@ return {
     vim.keymap.set('n', '<A-j>', require('smart-splits').resize_down)
     vim.keymap.set('n', '<A-k>', require('smart-splits').resize_up)
     vim.keymap.set('n', '<A-l>', require('smart-splits').resize_right)
-    -- moving between splits
-    vim.keymap.set('n', '<C-h>', require('smart-splits').move_cursor_left)
-    vim.keymap.set('n', '<C-j>', require('smart-splits').move_cursor_down)
-    vim.keymap.set('n', '<C-k>', require('smart-splits').move_cursor_up)
-    vim.keymap.set('n', '<C-l>', require('smart-splits').move_cursor_right)
+    -- moving between splits (normal and terminal modes)
+    vim.keymap.set({ 'n', 't' }, '<C-h>', require('smart-splits').move_cursor_left)
+    vim.keymap.set({ 'n', 't' }, '<C-j>', require('smart-splits').move_cursor_down)
+    vim.keymap.set({ 'n', 't' }, '<C-k>', require('smart-splits').move_cursor_up)
+    vim.keymap.set({ 'n', 't' }, '<C-l>', require('smart-splits').move_cursor_right)
     vim.keymap.set('n', '<C-\\>', require('smart-splits').move_cursor_previous)
     -- swapping buffers between windows
     vim.keymap.set('n', '<leader><leader>h', require('smart-splits').swap_buf_left)
