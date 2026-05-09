a6397d5aeb75627b2c1882665a5f099d2b18b838
Kareem Hepburn <kareem@hepburn.info>
2026-05-09 20:47:01 +0000

    refactor(nvim): use smart-splits for terminal window navigation
    - Moved <C-h/j/k/l> terminal mappings from keymaps.lua to smart-splits
    - Now uses smart-splits in both normal and terminal modes
    - Provides consistent behavior and tmux integration

--- private_dot_config/exact_nvim/exact_lua/exact_config/keymaps.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_config/keymaps.lua
@@ -245,12 +245,9 @@ map('n', '<leader>uI', '<cmd>InspectTree<cr>', { desc = 'Inspect Tree' })
 
 -- Terminal Mappings
 map('t', '<esc><esc>', '<c-\\><c-n>', { desc = 'Enter Normal Mode' })
-map('t', '<C-h>', '<cmd>wincmd h<cr>', { desc = 'Go to Left Window' })
-map('t', '<C-j>', '<cmd>wincmd j<cr>', { desc = 'Go to Lower Window' })
-map('t', '<C-k>', '<cmd>wincmd k<cr>', { desc = 'Go to Upper Window' })
-map('t', '<C-l>', '<cmd>wincmd l<cr>', { desc = 'Go to Right Window' })
 map('t', '<C-/>', '<cmd>close<cr>', { desc = 'Hide Terminal' })
 map('t', '<c-_>', '<cmd>close<cr>', { desc = 'which_key_ignore' })
+-- Terminal window navigation handled by smart-splits.lua
 
 -- windows
 map('n', '<leader>ww', '<C-W>p', { desc = 'Other Window', remap = true })
