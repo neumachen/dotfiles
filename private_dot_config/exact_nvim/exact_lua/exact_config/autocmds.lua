2f769646d3039347a2457958114cdd11eaf83227
neumachen <kareem@hepburn.info>
2026-06-24 18:07:05 +0000

    feat(nvim): enable line wrap globally
    Remove the filetype-specific wrap_spell autocmd that wasn't working
    (it used glob patterns like *.txt instead of filetype names).
    Global wrap with linebreak=true gives consistent soft-wrap behavior.

--- private_dot_config/exact_nvim/exact_lua/exact_config/autocmds.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_config/autocmds.lua
@@ -185,23 +185,6 @@ vim.api.nvim_create_autocmd('FileType', {
   callback = function(event) vim.bo[event.buf].buflisted = false end,
 })
 
--- wrap and check for spell in text filetypes
-vim.api.nvim_create_autocmd('FileType', {
-  group = augroup('wrap_spell'),
-  pattern = {
-    '*.txt',
-    '*.tex',
-    '*.typ',
-    'gitcommit',
-    'markdown',
-    'markdown.mdc',
-  },
-  callback = function()
-    vim.opt_local.wrap = true
-    vim.opt_local.spell = true
-  end,
-})
-
 -- Fix conceallevel for json files
 vim.api.nvim_create_autocmd({ 'FileType' }, {
   group = augroup('json_conceal'),
